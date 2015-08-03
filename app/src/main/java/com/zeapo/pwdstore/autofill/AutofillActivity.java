51a05087e5a349afa3f4e0febdc3a339179e9bbd
Matthew Wong <wongma@protonmail.ch>
2015-08-03 08:31:23 -0400

    Add searching for apps in autofill settings page

--- app/src/main/java/com/zeapo/pwdstore/autofill/AutofillActivity.java
+++ app/src/main/java/com/zeapo/pwdstore/autofill/AutofillActivity.java
@@ -7,11 +7,19 @@ import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.zeapo.pwdstore.R;
 
@@ -48,9 +56,15 @@ public class AutofillActivity extends AppCompatActivity {
             return;
         }
         // otherwise if called from settings
-        final PackageManager pm = getPackageManager();
-        List<ApplicationInfo> allApps = pm.getInstalledApplications(0);
+        setContentView(R.layout.autofill_recycler_view);
+        recyclerView = (RecyclerView) findViewById(R.id.autofill_recycler);
 
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // apps for which the user has custom settings should be in the recycler
+        final PackageManager pm = getPackageManager();
+        final List<ApplicationInfo> allApps = pm.getInstalledApplications(0);
         SharedPreferences prefs
                 = getSharedPreferences("autofill", Context.MODE_PRIVATE);
         Map<String, ?> prefApps = prefs.getAll();
@@ -60,18 +74,54 @@ public class AutofillActivity extends AppCompatActivity {
                 apps.add(applicationInfo);
             }
         }
-
-        setContentView(R.layout.autofill_recycler_view);
-        recyclerView = (RecyclerView) findViewById(R.id.autofill_recycler);
-
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
         recyclerAdapter = new AutofillRecyclerAdapter(apps, pm);
         recyclerView.setAdapter(recyclerAdapter);
 
-        setTitle("Autofill Apps");
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        final SearchView searchView = (SearchView) findViewById(R.id.app_search);
 
+        // create search suggestions of apps with icons & names
+        final SimpleCursorAdapter.ViewBinder viewBinder = new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (view instanceof TextView) {
+                    ((TextView) view).setText(cursor.getString(columnIndex));
+                } else if (view instanceof ImageView) {
+                    try {
+                        ((ImageView) view).setImageDrawable(pm.getApplicationIcon(cursor.getString(columnIndex)));
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // should be a better/faster way to do this?
+                MatrixCursor matrixCursor = new MatrixCursor(new String[]{"_id", "package", "label"});
+                for (ApplicationInfo applicationInfo : allApps) {
+                    if (applicationInfo.loadLabel(pm).toString().toLowerCase().contains(newText.toLowerCase())) {
+                        matrixCursor.addRow(new Object[]{0, applicationInfo.packageName, applicationInfo.loadLabel(pm)});
+                    }
+                }
+                SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(AutofillActivity.this
+                        , R.layout.app_list_item, matrixCursor, new String[]{"package", "label"}
+                        , new int[]{android.R.id.icon1, android.R.id.text1}, 0);
+                simpleCursorAdapter.setViewBinder(viewBinder);
+                searchView.setSuggestionsAdapter(simpleCursorAdapter);
+                return false;
+            }
+        });
+
+        setTitle("Autofill Apps");
     }
 
     @Override
