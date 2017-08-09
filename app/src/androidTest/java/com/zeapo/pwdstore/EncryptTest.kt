f074568018090ff4640690d4f4f60774496d8fb6
zeapo <mohamed@zenadi.com>
2017-08-09 21:54:55 +0200

    check that encryption works

--- app/src/androidTest/java/com/zeapo/pwdstore/EncryptTest.kt
+++ app/src/androidTest/java/com/zeapo/pwdstore/EncryptTest.kt
@@ -8,15 +8,13 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.zeapo.pwdstore.crypto.PgpActivity
-import kotlinx.android.synthetic.main.encrypt_layout.*
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -58,35 +56,26 @@ class EncryptTest {
         activity = mActivityRule.launchActivity(intent)
     }
 
-    @Test
-    fun activityShouldShowCategory() {
-        init()
-
-        val categoryView = activity.crypto_password_category
-        assertNotNull(categoryView)
-        assertEquals(parentPath, categoryView.text.toString())
-    }
-
     @SuppressLint("ApplySharedPref", "SetTextI18n")
     @Test
     fun shouldEncrypt() {
         init()
 
+        onView(withId(R.id.crypto_password_category)).check(ViewAssertions.matches(withText(parentPath)))
         activity.onBound(null)
         val clearPass = IOUtils.toString(testContext.assets.open("clear-store/category/sub"), Charsets.UTF_8.name())
         val passEntry = PasswordEntry(clearPass)
-        val encryptedEntry = IOUtils.toByteArray(testContext.assets.open("clear-store/category/sub"))
 
-        onView(withId(R.id.crypto_password_file_edit)).perform(typeText("category/sub"))
+        onView(withId(R.id.crypto_password_file_edit)).perform(typeText("sub"))
         onView(withId(R.id.crypto_password_edit)).perform(typeText(passEntry.password))
         onView(withId(R.id.crypto_extra_edit)).perform(scrollTo(), click())
         onView(withId(R.id.crypto_extra_edit)).perform(typeText(passEntry.extraContent))
 
         // we should return to the home screen once we confirm
-        onView(withId(R.id.crypto_confirm_add)).perform(click()).check(ViewAssertions.matches(withId(R.id.fab)))
+        onView(withId(R.id.crypto_confirm_add)).perform(click())
 
-        val resultEntry = FileUtils.readFileToByteArray(File("$path/$name.gpg"))
-
+        // The resulting file should exist
+        assert(File("$path/$name.gpg").exists())
     }
 }
 
