9b30d45d40762c30feccbe2a956a203c5dcb5537
Harsh Shandilya <msfjarvis@gmail.com>
2018-07-26 11:05:05 +0530

    Convert ToCloneOrNot to Kotlin
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ app/src/main/java/com/zeapo/pwdstore/ToCloneOrNot.kt
@@ -0,0 +1,18 @@
+package com.zeapo.pwdstore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ToCloneOrNot : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_to_clone_or_not, container, false)
+    }
+
+}
