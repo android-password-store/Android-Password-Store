46c3f96e8de15403cdc246105997f089a4ed0ac0
Valentin Brosseau <c4software@gmail.com>
2019-10-01 14:28:28 +0200

    Show item & filter it

---
+++ app/src/main/java/com/zeapo/pwdstore/autofill/v2/ui/holder/PasswordViewHolder.kt
@@ -0,0 +1,13 @@
+package com.zeapo.pwdstore.autofill.v2.ui.holder
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.zeapo.pwdstore.R
+
+class PasswordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val type_image: ImageView = itemView.findViewById(R.id.type_image)
+    val type: TextView = itemView.findViewById(R.id.type)
+    val label: TextView = itemView.findViewById(R.id.label)
+}
--- app/src/main/java/com/zeapo/pwdstore/utils/Extensions.kt
+++ app/src/main/java/com/zeapo/pwdstore/utils/Extensions.kt
@@ -1,5 +1,23 @@
 package com.zeapo.pwdstore.utils
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
 fun String.splitLines(): Array<String> {
     return split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+    })
 }
