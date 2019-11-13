b9efe1920fdd0c993c4eb9f0b8fcb6ca3ced898c
Harsh Shandilya <msfjarvis@gmail.com>
2019-11-13 23:29:10 +0530

    Respect hidden folders setting in child count
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/zeapo/pwdstore/ui/adapters/EntryRecyclerAdapter.kt
+++ app/src/main/java/com/zeapo/pwdstore/ui/adapters/EntryRecyclerAdapter.kt
@@ -4,11 +4,13 @@
  */
 package com.zeapo.pwdstore.ui.adapters
 
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import com.zeapo.pwdstore.R
 import com.zeapo.pwdstore.utils.PasswordItem
@@ -19,6 +21,7 @@ import java.util.TreeSet
 
 abstract class EntryRecyclerAdapter internal constructor(val values: ArrayList<PasswordItem>) : RecyclerView.Adapter<EntryRecyclerAdapter.ViewHolder>() {
     internal val selectedItems: MutableSet<Int> = TreeSet()
+    private lateinit var settings: SharedPreferences
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount(): Int {
@@ -77,12 +80,19 @@ abstract class EntryRecyclerAdapter internal constructor(val values: ArrayList<P
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val pass = values[position]
+        if (!::settings.isInitialized) {
+            settings = PreferenceManager.getDefaultSharedPreferences(holder.view.context.applicationContext)
+        }
+        val showHidden = settings.getBoolean("show_hidden_folders", false)
         holder.name.text = pass.toString()
         if (pass.type == PasswordItem.TYPE_CATEGORY) {
             holder.type.visibility = View.GONE
             holder.typeImage.setImageResource(R.drawable.ic_multiple_files_24dp)
             holder.folderIndicator.visibility = View.VISIBLE
-            val childCount = (pass.file.list() ?: emptyArray<File>()).size
+            val children = pass.file.listFiles { pathname ->
+                !(!showHidden && (pathname.isDirectory && pathname.isHidden))
+            } ?: emptyArray<File>()
+            val childCount = children.size
             if (childCount > 0) {
                 holder.childCount.visibility = View.VISIBLE
                 holder.childCount.text = "$childCount"
