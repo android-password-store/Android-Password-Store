342ac8e4acbba6293ec47d175f295253739109bd
Aditya Wasan <adityawasan55@gmail.com>
2019-11-29 19:10:44 +0530

    Add splash activity and implement Android Q's Edge to Edge suggestions
    Co-authored-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ app/src/main/kotlin/com/passwordstore/android/activitiy/SplashActivity.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2014-2019 The Android Password Store Authors. All Rights Reserved.
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+package com.passwordstore.android.activitiy
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.passwordstore.android.databinding.ActivitySplashBinding
+import com.passwordstore.android.ui.EdgeToEdge
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        EdgeToEdge.setUpRoot(binding.root as ViewGroup)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/kotlin/com/passwordstore/android/ui/EdgeToEdge.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2014-2019 The Android Password Store Authors. All Rights Reserved.
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+package com.passwordstore.android.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+
+/**
+ * A utility for edge-to-edge display. It provides several features needed to make the app
+ * displayed edge-to-edge on Android Q with gestural navigation.
+ */
+object EdgeToEdge : EdgeToEdgeImpl by RealEdgeToEdge()
+
+private interface EdgeToEdgeImpl {
+
+    /**
+     * Configures a root view of an Activity in edge-to-edge display.
+     * @param root A root view of an Activity.
+     */
+    fun setUpRoot(root: ViewGroup) {}
+
+    /**
+     * Configures an app bar and a toolbar for edge-to-edge display.
+     * @param appBar An [AppBarLayout].
+     * @param toolbar A [Toolbar] in the [appBar].
+     */
+    fun setUpAppBar(appBar: AppBarLayout, toolbar: Toolbar) {}
+
+    /**
+     * Configures a scrolling content for edge-to-edge display.
+     * @param scrollingContent A scrolling ViewGroup. This is typically a RecyclerView or a
+     * ScrollView. It should be as wide as the screen, and should touch the bottom edge of
+     * the screen.
+     * @param fab A [ExtendedFloatingActionButton] to show last item on scrolling ViewGroup above fab.
+     */
+    fun setUpScrollingContent(scrollingContent: ViewGroup, fab: ExtendedFloatingActionButton?) {}
+
+    /**
+     * Configures a floating action button for edge-to-edge display.
+     * @param fab An [ExtendedFloatingActionButton].
+     */
+    fun setUpFAB(fab: ExtendedFloatingActionButton) {}
+}
+
+@RequiresApi(21)
+private class RealEdgeToEdge : EdgeToEdgeImpl {
+
+    override fun setUpRoot(root: ViewGroup) {
+        root.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    override fun setUpScrollingContent(scrollingContent: ViewGroup, fab: ExtendedFloatingActionButton?) {
+        val originalPaddingLeft = scrollingContent.paddingLeft
+        val originalPaddingRight = scrollingContent.paddingRight
+        val originalPaddingBottom = scrollingContent.paddingBottom
+
+        val fabPaddingBottom = fab?.height ?: 0
+
+        val originalMarginTop = scrollingContent.marginTop
+
+        scrollingContent.setOnApplyWindowInsetsListener { _, windowInsets ->
+            scrollingContent.updatePadding(
+                    left = originalPaddingLeft + windowInsets.systemWindowInsetLeft,
+                    right = originalPaddingRight + windowInsets.systemWindowInsetRight,
+                    bottom = originalPaddingBottom + fabPaddingBottom + windowInsets.systemWindowInsetBottom
+            )
+            scrollingContent.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = originalMarginTop + windowInsets.systemWindowInsetTop
+            }
+            windowInsets
+        }
+    }
+
+    override fun setUpFAB(fab: ExtendedFloatingActionButton) {
+        val originalMarginLeft = fab.marginLeft
+        val originalMarginRight = fab.marginRight
+        val originalMarginBottom = fab.marginBottom
+        fab.setOnApplyWindowInsetsListener { _, windowInsets ->
+            fab.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = originalMarginLeft + windowInsets.systemWindowInsetLeft
+                rightMargin = originalMarginRight + windowInsets.systemWindowInsetRight
+                bottomMargin = originalMarginBottom + windowInsets.systemWindowInsetBottom
+            }
+            windowInsets
+        }
+    }
+}
