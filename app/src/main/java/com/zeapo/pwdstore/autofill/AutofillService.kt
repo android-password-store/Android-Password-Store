23195a27f95b04ed69d3ee7a42f7020a9cae105e
Aditya Wasan <adityawasan55@gmail.com>
2020-01-30 15:49:29 +0530

    Spotless fixes
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/src/main/java/com/zeapo/pwdstore/autofill/AutofillService.kt
+++ app/src/main/java/com/zeapo/pwdstore/autofill/AutofillService.kt
@@ -13,7 +13,6 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
 import android.provider.Settings
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.Window
