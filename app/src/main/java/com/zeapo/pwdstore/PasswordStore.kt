android-password-store/Android-Password-Store

commit fde8137b62a3d0cd8e4d94df6fde04f2f61cbbc9

Delete AutofillMatches when deleting files (#659)

app/src/main/java/com/zeapo/pwdstore/PasswordStore.kt
@@ -499,6 +499,8 @@ class PasswordStore : AppCompatActivity() {
         MaterialAlertDialogBuilder(this)
                 .setMessage(resources.getString(R.string.delete_dialog_text, item.longName))
                 .setPositiveButton(resources.getString(R.string.dialog_yes)) { _, _ ->
+                    val filesToDelete = FileUtils.listFiles(item.file, null, true)
+                    AutofillMatcher.updateMatches(applicationContext, delete = filesToDelete)
                     item.file.deleteRecursively()
                     adapter.remove(position)
                     it.remove()
@@ -665,7 +667,7 @@ class PasswordStore : AppCompatActivity() {
                             // TODO this should show a warning to the user
                             Timber.tag(TAG).e("Something went wrong while moving.")
                         } else {
-                            AutofillMatcher.updateMatchesFor(this, sourceDestinationMap)
+                            AutofillMatcher.updateMatches(this, sourceDestinationMap)
                             commitChange(this.resources
                                     .getString(
                                             R.string.git_commit_move_text,

app/src/main/java/com/zeapo/pwdstore/autofill/oreo/AutofillMatcher.kt
@@ -148,10 +148,11 @@ class AutofillMatcher {
 
         /**
          * Goes through all existing matches and updates their associated entries by using
-         * [sourceDestinationMap] as a lookup table.
+         * [moveFromTo] as a lookup table and deleting the matches for files in [delete].
          */
-        fun updateMatchesFor(context: Context, sourceDestinationMap: Map<File, File>) {
-            val oldNewPathMap = sourceDestinationMap.mapValues { it.value.absolutePath }
+        fun updateMatches(context: Context, moveFromTo: Map<File, File> = emptyMap(), delete: Collection<File> = emptyList()) {
+            val deletePathList = delete.map { it.absolutePath }
+            val oldNewPathMap = moveFromTo.mapValues { it.value.absolutePath }
                 .mapKeys { it.key.absolutePath }
             for (prefs in listOf(context.autofillAppMatches, context.autofillWebMatches)) {
                 for ((key, value) in prefs.all) {
@@ -164,7 +165,10 @@ class AutofillMatcher {
                     // Delete all matches for file locations that are going to be overwritten, then
                     // transfer matches over to the files at their new locations.
                     val newMatches =
-                        oldMatches.asSequence().minus(oldNewPathMap.values).map { match ->
+                        oldMatches.asSequence()
+                            .minus(deletePathList)
+                            .minus(oldNewPathMap.values)
+                            .map { match ->
                             val newPath = oldNewPathMap[match] ?: return@map match
                             d { "Updating match for $key: $match --> $newPath" }
                             newPath

commit f2d3f1a12d2aba0697d12c64a95e784afc990a42

Fix deletion of individual password files
Commit fde8137b (#659) introduced a regression that results in Password
Store crashing when the user tries to delete a single password file as
opposed to a directory.

The root cause is a call of FileUtils.listFiles() on the selected item,
which only works for directories.

The fix is to work with a list consisting only of the selected item if
it happens to be a file.

app/src/main/java/com/zeapo/pwdstore/PasswordStore.kt
@@ -499,7 +499,11 @@ class PasswordStore : AppCompatActivity() {
         MaterialAlertDialogBuilder(this)
                 .setMessage(resources.getString(R.string.delete_dialog_text, item.longName))
                 .setPositiveButton(resources.getString(R.string.dialog_yes)) { _, _ ->
-                    val filesToDelete = FileUtils.listFiles(item.file, null, true)
+                    val filesToDelete = if (item.file.isDirectory) {
+                        FileUtils.listFiles(item.file, null, true)
+                    } else {
+                        listOf(item.file)
+                    }
                     AutofillMatcher.updateMatches(applicationContext, delete = filesToDelete)
                     item.file.deleteRecursively()
                     adapter.remove(position)