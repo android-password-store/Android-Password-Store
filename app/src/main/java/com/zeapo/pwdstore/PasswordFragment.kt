6104a4dd39aadd2fba89102a34651f7404694c13
Fabian Henneke <fabian@henneke.me>
2020-04-06 15:48:58 +0200

    Organize imports

--- app/src/main/java/com/zeapo/pwdstore/PasswordFragment.kt
+++ app/src/main/java/com/zeapo/pwdstore/PasswordFragment.kt
@@ -13,7 +13,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
