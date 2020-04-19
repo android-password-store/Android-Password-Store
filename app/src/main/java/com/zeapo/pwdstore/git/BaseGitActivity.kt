d30baba94642a83dba4165a429d710ff69419e04
Harsh Shandilya <me@msfjarvis.dev>
2020-04-19 16:49:54 +0530

    Use java.net.URL for saner parsing
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/git/BaseGitActivity.kt
+++ app/src/main/java/com/zeapo/pwdstore/git/BaseGitActivity.kt
@@ -20,6 +20,7 @@ import com.zeapo.pwdstore.git.config.SshApiSessionFactory
 import com.zeapo.pwdstore.utils.PasswordRepository
 import com.zeapo.pwdstore.utils.getEncryptedPrefs
 import java.io.File
+import java.net.URL
 import timber.log.Timber
 
 /**
@@ -103,9 +104,10 @@ abstract class BaseGitActivity : AppCompatActivity() {
                 val portPart =
                     if (serverPort == "443" || serverPort.isEmpty()) "" else ":$serverPort"
                 var result = "$hostnamePart$portPart$pathPart"
-                result = when {
-                    result.startsWith("http://") -> result.replace("http:", "https:")
-                    !result.startsWith("http") -> "https://$result"
+                val url = URL(result)
+                result = when (url.protocol) {
+                    null -> "https://$result"
+                    "http" -> result.replace("http:", "https:")
                     else -> result
                 }
                 result
