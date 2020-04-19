7ce883140139cf9f293d690129601349d6d9a4d0
Harsh Shandilya <msfjarvis@gmail.com>
2020-04-19 16:52:14 +0530

    release/deploy-snapshot: Don't swallow errors (#724)
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-snapshot.sh
+++ release/deploy-snapshot.sh
@@ -7,5 +7,5 @@ chmod 600 "$SSHDIR/key"
 export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
 cd "$GITHUB_WORKSPACE/app/build/outputs/apk/release"
 rm output.json
-rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING" || true
+rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING" || exit 1
 exit 0
