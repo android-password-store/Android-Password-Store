f7e405486f4aa092ae2b164703fd50149910e235
Harsh Shandilya <me@msfjarvis.dev>
2020-04-21 14:00:33 +0530

    release/deploy-github: don't attempt to tag before deployment
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-github.sh
+++ release/deploy-github.sh
@@ -3,7 +3,6 @@
 trap 'exit 1' SIGINT SIGTERM
 
 [ -z "$(command -v hub)" ] && { echo "hub not installed; aborting!"; exit 1; }
-TAG="${1}"
-hub tag -afs "${TAG:?}"
+[ -z "${1}" ] && { echo "No tag specified!"; exit 1; }
 gradle clean bundleRelease assembleRelease
 hub release create "${TAG}" -a app/build/outputs/apk/release/app-release.apk
