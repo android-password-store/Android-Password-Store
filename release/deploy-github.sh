a5437c480ab189e0a6af86aae638e2e67d091831
Harsh Shandilya <me@msfjarvis.dev>
2020-04-23 13:09:03 +0530

    release/deploy-github: fix tag parameter
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-github.sh
+++ release/deploy-github.sh
@@ -5,4 +5,4 @@ trap 'exit 1' SIGINT SIGTERM
 [ -z "$(command -v hub)" ] && { echo "hub not installed; aborting!"; exit 1; }
 [ -z "${1}" ] && { echo "No tag specified!"; exit 1; }
 gradle clean bundleRelease assembleRelease
-hub release create "${TAG}" -a app/build/outputs/apk/release/app-release.apk
+hub release create "${1}" -a app/build/outputs/apk/release/app-release.apk
