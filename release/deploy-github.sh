4c3ccb42cd2c88ae8600f046435c38f5231625bb
Harsh Shandilya <me@msfjarvis.dev>
2020-04-29 03:21:58 +0530

    release/deploy-github: automatically switch refs
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-github.sh
+++ release/deploy-github.sh
@@ -4,5 +4,8 @@ trap 'exit 1' SIGINT SIGTERM
 
 [ -z "$(command -v hub)" ] && { echo "hub not installed; aborting!"; exit 1; }
 [ -z "${1}" ] && { echo "No tag specified!"; exit 1; }
+prev_ref="$(git rev-parse --abbrev-ref HEAD)"
+git checkout "${1}"
 gradle clean bundleRelease assembleRelease
 hub release create "${1}" -a app/build/outputs/apk/release/app-release.apk
+git checkout "$prev_ref"
