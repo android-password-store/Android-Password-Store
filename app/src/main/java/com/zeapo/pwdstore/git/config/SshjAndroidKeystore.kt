573a5e6916632a182a8adc31b182f5dbe255ee10
Fabian Henneke <fabian@henneke.me>
2020-05-31 17:07:03 +0200

    Refactor Keystore generation and add StrongBox support

--- app/src/main/java/com/zeapo/pwdstore/git/config/SshjAndroidKeystore.kt
+++ app/src/main/java/com/zeapo/pwdstore/git/config/SshjAndroidKeystore.kt
@@ -4,9 +4,14 @@
  */
 package com.zeapo.pwdstore.git.config
 
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyInfo
 import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.security.keystore.StrongBoxUnavailableException
 import com.github.ajalt.timberkt.e
+import com.github.ajalt.timberkt.i
 import net.schmizz.sshj.common.Buffer
 import net.schmizz.sshj.common.KeyType
 import net.schmizz.sshj.common.SSHRuntimeException
@@ -24,7 +29,10 @@ import java.security.GeneralSecurityException
 import java.security.InvalidKeyException
 import java.security.Key
 import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.KeyPairGenerator
 import java.security.KeyStore
+import java.security.KeyStoreException
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.SignatureException
@@ -40,6 +48,73 @@ private val androidKeystore: KeyStore by lazy {
 private fun KeyStore.getPrivateKey(keyAlias: String) = getKey(keyAlias, null) as? PrivateKey
 
 private fun KeyStore.getPublicKey(keyAlias: String) = getCertificate(keyAlias)?.publicKey
+
+enum class SshKeyGenType(private val algorithm: String, private val keyLength: Int,
+                         private val applyToSpec: KeyGenParameterSpec.Builder.() -> Unit) {
+
+    Rsa2048(KeyProperties.KEY_ALGORITHM_RSA, 2048, {
+        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+    }),
+    Rsa3072(KeyProperties.KEY_ALGORITHM_RSA, 3072, {
+        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+    }),
+    Rsa4096(KeyProperties.KEY_ALGORITHM_RSA, 4096, {
+        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+    }),
+    Ecdsa256(KeyProperties.KEY_ALGORITHM_EC, 256, {
+        setAlgorithmParameterSpec(java.security.spec.ECGenParameterSpec("secp256r1"))
+        setDigests(KeyProperties.DIGEST_SHA256)
+    }),
+    Ecdsa384(KeyProperties.KEY_ALGORITHM_EC, 384, {
+        setAlgorithmParameterSpec(java.security.spec.ECGenParameterSpec("secp384r1"))
+        setDigests(KeyProperties.DIGEST_SHA384)
+    }),
+    Ecdsa521(KeyProperties.KEY_ALGORITHM_EC, 521, {
+        setAlgorithmParameterSpec(java.security.spec.ECGenParameterSpec("secp521r1"))
+        setDigests(KeyProperties.DIGEST_SHA512)
+    });
+
+    private fun generateKeyPair(keyAlias: String, requireAuthentication: Boolean, useStrongBox: Boolean): KeyPair {
+        val parameterSpec = KeyGenParameterSpec.Builder(
+            keyAlias, KeyProperties.PURPOSE_SIGN
+        ).run {
+            setKeySize(keyLength)
+            apply(applyToSpec)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                setIsStrongBoxBacked(useStrongBox)
+            }
+            if (requireAuthentication) {
+                setUserAuthenticationRequired(true)
+                // 60 seconds should provide ample time to connect to the SSH server and
+                // perform authentication (and possibly a Git operation and another connect,
+                // in case of the clone operation).
+                setUserAuthenticationValidityDurationSeconds(60)
+            }
+            build()
+        }
+        return KeyPairGenerator.getInstance(algorithm, PROVIDER_ANDROID_KEY_STORE).run {
+            initialize(parameterSpec)
+            generateKeyPair()
+        }
+    }
+
+    fun generateKeyPair(keyAlias: String, requireAuthentication: Boolean): KeyPair {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            try {
+                return generateKeyPair(
+                    keyAlias, requireAuthentication = requireAuthentication, useStrongBox = true)
+            } catch (error: Exception) {
+                i { "Falling back to non-StrongBox Keystore key" }
+            }
+        }
+        return generateKeyPair(
+            keyAlias, requireAuthentication = requireAuthentication, useStrongBox = false)
+    }
+}
+
 
 class AndroidKeystoreKeyProvider(private val keyAlias: String) : KeyProvider {
 
--- app/src/main/java/com/zeapo/pwdstore/sshkeygen/SshKeyGenFragment.kt
+++ app/src/main/java/com/zeapo/pwdstore/sshkeygen/SshKeyGenFragment.kt
@@ -5,8 +5,6 @@
 package com.zeapo.pwdstore.sshkeygen
 
 import android.os.Bundle
-import android.security.keystore.KeyGenParameterSpec
-import android.security.keystore.KeyProperties
 import android.security.keystore.UserNotAuthenticatedException
 import android.util.Base64
 import android.view.View
@@ -20,7 +18,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.zeapo.pwdstore.R
 import com.zeapo.pwdstore.databinding.FragmentSshKeygenBinding
 import com.zeapo.pwdstore.git.ANDROID_KEYSTORE_ALIAS_SSH_KEY
-import com.zeapo.pwdstore.git.config.PROVIDER_ANDROID_KEY_STORE
+import com.zeapo.pwdstore.git.config.SshKeyGenType
 import com.zeapo.pwdstore.utils.BiometricAuthenticator
 import com.zeapo.pwdstore.utils.keyguardManager
 import com.zeapo.pwdstore.utils.viewBinding
@@ -30,43 +28,12 @@ import kotlinx.coroutines.withContext
 import net.schmizz.sshj.common.Buffer
 import net.schmizz.sshj.common.KeyType
 import java.io.File
-import java.security.KeyPairGenerator
-import java.security.spec.ECGenParameterSpec
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-enum class KeyGenType(val algorithm: String, val keyLength: Int,
-                      val applyToSpec: KeyGenParameterSpec.Builder.() -> Unit) {
-
-    Rsa2048(KeyProperties.KEY_ALGORITHM_RSA, 2048, {
-        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-    }),
-    Rsa3072(KeyProperties.KEY_ALGORITHM_RSA, 3072, {
-        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-    }),
-    Rsa4096(KeyProperties.KEY_ALGORITHM_RSA, 4096, {
-        setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-        setDigests(KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-    }),
-    Ecdsa256(KeyProperties.KEY_ALGORITHM_EC, 256, {
-        setAlgorithmParameterSpec(ECGenParameterSpec("secp256r1"))
-        setDigests(KeyProperties.DIGEST_SHA256)
-    }),
-    Ecdsa384(KeyProperties.KEY_ALGORITHM_EC, 384, {
-        setAlgorithmParameterSpec(ECGenParameterSpec("secp384r1"))
-        setDigests(KeyProperties.DIGEST_SHA384)
-    }),
-    Ecdsa521(KeyProperties.KEY_ALGORITHM_EC, 521, {
-        setAlgorithmParameterSpec(ECGenParameterSpec("secp521r1"))
-        setDigests(KeyProperties.DIGEST_SHA512)
-    }),
-}
-
 class SshKeyGenFragment : Fragment(R.layout.fragment_ssh_keygen) {
 
-    private var keyType = KeyGenType.Ecdsa384
+    private var keyType = SshKeyGenType.Ecdsa384
     private val binding by viewBinding(FragmentSshKeygenBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -79,9 +46,10 @@ class SshKeyGenFragment : Fragment(R.layout.fragment_ssh_keygen) {
             keyLengthGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
                 if (isChecked) {
                     keyType = when (checkedId) {
-                        R.id.key_type_rsa_2048 -> KeyGenType.Rsa2048
-                        R.id.key_type_rsa_3072 -> KeyGenType.Rsa3072
-                        R.id.key_type_ecdsa_384 -> KeyGenType.Ecdsa384
+                        R.id.key_type_rsa_2048 -> SshKeyGenType.Rsa2048
+                        R.id.key_type_rsa_3072 -> SshKeyGenType.Rsa3072
+                        R.id.key_type_ecdsa_256 -> SshKeyGenType.Ecdsa256
+                        R.id.key_type_ecdsa_384 -> SshKeyGenType.Ecdsa384
                         else -> throw IllegalStateException("Invalid key type selection")
                     }
                 }
@@ -92,12 +60,8 @@ class SshKeyGenFragment : Fragment(R.layout.fragment_ssh_keygen) {
         }
     }
 
-    private fun generateAndStoreKey(parameterSpec: KeyGenParameterSpec, comment: String) {
-        val keyPair = KeyPairGenerator.getInstance(keyType.algorithm,
-            PROVIDER_ANDROID_KEY_STORE).run {
-            initialize(parameterSpec)
-            generateKeyPair()
-        }
+    private fun generateAndStoreKey(requireAuthentication: Boolean, comment: String) {
+        val keyPair = keyType.generateKeyPair(ANDROID_KEYSTORE_ALIAS_SSH_KEY, requireAuthentication)
         val keyType = KeyType.fromKey(keyPair.public)
         val rawPublicKey = Buffer.PlainBuffer().run {
             keyType.putPubKeyIntoBuffer(keyPair.public, this)
@@ -119,23 +83,8 @@ class SshKeyGenFragment : Fragment(R.layout.fragment_ssh_keygen) {
         }
         val e = try {
             withContext(Dispatchers.IO) {
-                val createWithUserAuthentication = binding.keyRequireAuthentication.isChecked
-                val parameterSpec = KeyGenParameterSpec.Builder(
-                    ANDROID_KEYSTORE_ALIAS_SSH_KEY,
-                    KeyProperties.PURPOSE_SIGN
-                ).run {
-                    setKeySize(keyType.keyLength)
-                    apply(keyType.applyToSpec)
-                    if (createWithUserAuthentication) {
-                        setUserAuthenticationRequired(true)
-                        // 60 seconds should provide ample time to connect to the SSH server and
-                        // perform authentication (and possibly a Git operation and another connect,
-                        // in case of the clone operation).
-                        setUserAuthenticationValidityDurationSeconds(60)
-                    }
-                    build()
-                }
-                if (createWithUserAuthentication) {
+                val requireAuthentication = binding.keyRequireAuthentication.isChecked
+                if (requireAuthentication) {
                     val result = withContext(Dispatchers.Main) {
                         suspendCoroutine<BiometricAuthenticator.Result> { cont ->
                             BiometricAuthenticator.authenticate(requireActivity(), R.string.biometric_prompt_title_ssh_keygen) {
@@ -143,14 +92,10 @@ class SshKeyGenFragment : Fragment(R.layout.fragment_ssh_keygen) {
                             }
                         }
                     }
-                    if (result is BiometricAuthenticator.Result.Success) {
-                        generateAndStoreKey(parameterSpec, comment)
-                    } else {
+                    if (result !is BiometricAuthenticator.Result.Success)
                         throw UserNotAuthenticatedException(getString(R.string.biometric_auth_generic_failure))
-                    }
-                } else {
-                    generateAndStoreKey(parameterSpec, comment)
                 }
+                generateAndStoreKey(requireAuthentication, comment)
             }
             null
         } catch (e: Exception) {
