4dca178c673903cec012125b8ba8779648825f52
Harsh Shandilya <me@msfjarvis.dev>
2020-06-09 19:15:43 +0530

    FileUtils: convert to object
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/utils/FileUtils.kt
+++ app/src/main/java/com/zeapo/pwdstore/utils/FileUtils.kt
@@ -8,126 +8,122 @@ import java.io.IOException
 import java.io.InputStream
 import java.nio.charset.Charset
 
-class FileUtils {
-    companion object {
+object FileUtils {
+    @JvmStatic
+    fun listFiles(dir: File, extensions: Array<String>?, recursive: Boolean): Collection<File> {
+        val res = ArrayList<File>()
+        val files = dir.listFiles()
 
-        @JvmStatic
-        fun listFiles(dir: File, extensions: Array<String>?, recursive: Boolean): Collection<File> {
-            val res = ArrayList<File>()
-            val files = dir.listFiles()
+        if (files != null && files.isNotEmpty()) {
 
-            if (files != null && files.isNotEmpty()) {
-
-                files.forEach { file ->
-                    // Check if the file is a directory and recursive add
-                    if (file.isDirectory && recursive) {
-                        res.addAll(listFiles(file, extensions, recursive))
-                    } else if (!file.isDirectory) {
-                        if (extensions == null) {
-                            //not check extension
-                            res.add(file)
-                        } else if (extensions.contains(file.extension)) {
-                            res.add(file)
-                        }
+            files.forEach { file ->
+                // Check if the file is a directory and recursive add
+                if (file.isDirectory && recursive) {
+                    res.addAll(listFiles(file, extensions, recursive))
+                } else if (!file.isDirectory) {
+                    if (extensions == null) {
+                        //not check extension
+                        res.add(file)
+                    } else if (extensions.contains(file.extension)) {
+                        res.add(file)
                     }
                 }
             }
-
-            return res
         }
 
-        @JvmStatic
-        fun readFileToString(file: File, charset: Charset): String {
-            return file.readText(charset)
-        }
+        return res
+    }
 
-        @JvmStatic
-        @Throws(IOException::class)
-        fun openInputStream(file: File): FileInputStream {
-            if (file.exists()) {
-                if (file.isDirectory) {
-                    throw IOException("File '$file' exists but is a directory")
-                }
-                if (!file.canRead()) {
-                    throw IOException("File '$file' cannot be read")
-                }
-            } else {
-                throw FileNotFoundException("File '$file' does not exist")
+    @JvmStatic
+    fun readFileToString(file: File, charset: Charset): String {
+        return file.readText(charset)
+    }
+
+    @JvmStatic
+    @Throws(IOException::class)
+    fun openInputStream(file: File): FileInputStream {
+        if (file.exists()) {
+            if (file.isDirectory) {
+                throw IOException("File '$file' exists but is a directory")
             }
-            return FileInputStream(file)
-        }
-
-        @JvmStatic
-        @Throws(IOException::class)
-        fun openOutputStream(file: File): FileOutputStream? {
-            if (file.exists()) {
-                if (file.isDirectory) {
-                    throw IOException("File '$file' exists but is a directory")
-                }
-                if (!file.canWrite()) {
-                    throw IOException("File '$file' cannot be written to")
-                }
-            } else {
-                val parent = file.parentFile
-                if (parent != null) {
-                    if (!parent.mkdirs() && !parent.isDirectory) {
-                        throw IOException("Directory '$parent' could not be created")
-                    }
-                }
+            if (!file.canRead()) {
+                throw IOException("File '$file' cannot be read")
             }
-            return FileOutputStream(file, false)
+        } else {
+            throw FileNotFoundException("File '$file' does not exist")
         }
+        return FileInputStream(file)
+    }
 
-        @JvmStatic
-        @Throws(IOException::class)
-        fun copyInputStreamToFile(source: InputStream?, destination: File) {
-            val output = openOutputStream(destination)
-            source!!.copyTo(output!!, 1024)
-        }
-
-        @JvmStatic
-        @Throws(IOException::class)
-        fun cleanDirectory(directory:File) {
-            val files = directory.listFiles()
-
-            files!!.forEach { file ->
-                //remove files inside directory
-                if (file.isDirectory)
-                    cleanDirectory(file)
-
-                //remove file or directory
-                if (!file.delete())
-                    throw IOException("Can't delete file ${file}")
+    @JvmStatic
+    @Throws(IOException::class)
+    fun openOutputStream(file: File): FileOutputStream? {
+        if (file.exists()) {
+            if (file.isDirectory) {
+                throw IOException("File '$file' exists but is a directory")
+            }
+            if (!file.canWrite()) {
+                throw IOException("File '$file' cannot be written to")
+            }
+        } else {
+            val parent = file.parentFile
+            if (parent != null) {
+                if (!parent.mkdirs() && !parent.isDirectory) {
+                    throw IOException("Directory '$parent' could not be created")
+                }
             }
         }
+        return FileOutputStream(file, false)
+    }
 
-        @JvmStatic
-        fun deleteQuietly(file: File?): Boolean {
-            return if (file != null ){
-                //remove files inside directory
-                if (file.isDirectory)
-                    cleanDirectory(file)
+    @JvmStatic
+    @Throws(IOException::class)
+    fun copyInputStreamToFile(source: InputStream?, destination: File) {
+        val output = openOutputStream(destination)
+        source!!.copyTo(output!!, 1024)
+    }
 
-                //remove file or directory
-                file.delete()
-            }else false
+    @JvmStatic
+    @Throws(IOException::class)
+    fun cleanDirectory(directory: File) {
+        val files = directory.listFiles()
+
+        files!!.forEach { file ->
+            //remove files inside directory
+            if (file.isDirectory)
+                cleanDirectory(file)
+
+            //remove file or directory
+            if (!file.delete())
+                throw IOException("Can't delete file $file")
         }
+    }
 
-        @JvmStatic
-        fun getBaseName(filename: String): String {
-            //take file's name with extension
-            val indexName = filename.lastIndexOf('/')
-            val nameWithExtension = filename.substring(indexName + 1)
+    @JvmStatic
+    fun deleteQuietly(file: File?): Boolean {
+        return if (file != null) {
+            //remove files inside directory
+            if (file.isDirectory)
+                cleanDirectory(file)
 
-            //only send file's name
-            val indexExt = nameWithExtension.lastIndexOf('.')
+            //remove file or directory
+            file.delete()
+        } else false
+    }
 
-            //if file doesn't have extension (i.e it's a folder)
-            return if (indexExt == -1 )
-                nameWithExtension
-            else
-                nameWithExtension.substring(0, indexExt)
-        }
+    @JvmStatic
+    fun getBaseName(filename: String): String {
+        //take file's name with extension
+        val indexName = filename.lastIndexOf('/')
+        val nameWithExtension = filename.substring(indexName + 1)
 
+        //only send file's name
+        val indexExt = nameWithExtension.lastIndexOf('.')
+
+        //if file doesn't have extension (i.e it's a folder)
+        return if (indexExt == -1)
+            nameWithExtension
+        else
+            nameWithExtension.substring(0, indexExt)
     }
 }
