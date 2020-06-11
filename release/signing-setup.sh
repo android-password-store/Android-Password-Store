d98cf0487805f5c3b9daec757a910ad2b4b42721
Harsh Shandilya <me@msfjarvis.dev>
2020-06-12 03:18:37 +0530

    release: re-encrypt secrets with updated script
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/signing-setup.sh
+++ release/signing-setup.sh
@@ -4,10 +4,10 @@ ENCRYPT_KEY=$1
 
 if [[ -n "$ENCRYPT_KEY" ]]; then
     # Decrypt Release key
-    openssl enc -aes-256-cbc -md sha256 -d -in release/keystore.cipher -out keystore.jks -k "${ENCRYPT_KEY}"
+    openssl enc -aes-256-cbc -md sha256 -pbkdf2 -d -in release/keystore.cipher -out keystore.jks -k "${ENCRYPT_KEY}"
 
     # Decrypt signing config
-    openssl enc -aes-256-cbc -md sha256 -d -in release/props.cipher -out keystore.properties -k "${ENCRYPT_KEY}"
+    openssl enc -aes-256-cbc -md sha256 -pbkdf2 -d -in release/props.cipher -out keystore.properties -k "${ENCRYPT_KEY}"
 else
     echo "ENCRYPT_KEY is empty"
 fi
