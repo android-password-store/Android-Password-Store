960973131fa8e09f1fd46e382214ea8aa91f6384
Harsh Shandilya <me@msfjarvis.dev>
2020-06-27 15:04:47 +0530

    Simplify tests
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/test/java/com/zeapo/pwdstore/model/PasswordEntryTest.kt
+++ app/src/test/java/com/zeapo/pwdstore/model/PasswordEntryTest.kt
@@ -12,72 +12,76 @@ import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class PasswordEntryTest {
-    // This implementation just sends back defaults for now.
-    private val testFinder = object : TotpFinder {
-        override fun findSecret(content: String): String? {
-            return ""
-        }
-
-        override fun findDigits(content: String): String {
-            return "6"
-        }
-
-        override fun findPeriod(content: String): Long {
-            return 30
-        }
-
-        override fun findAlgorithm(content: String): String {
-            return "HmacSHA1"
-        }
-    }
+    private fun makeEntry(content: String) = PasswordEntry(content, testFinder)
 
     @Test fun testGetPassword() {
-        assertEquals("fooooo", PasswordEntry("fooooo\nbla\n", testFinder).password)
-        assertEquals("fooooo", PasswordEntry("fooooo\nbla", testFinder).password)
-        assertEquals("fooooo", PasswordEntry("fooooo\n", testFinder).password)
-        assertEquals("fooooo", PasswordEntry("fooooo", testFinder).password)
-        assertEquals("", PasswordEntry("\nblubb\n", testFinder).password)
-        assertEquals("", PasswordEntry("\nblubb", testFinder).password)
-        assertEquals("", PasswordEntry("\n", testFinder).password)
-        assertEquals("", PasswordEntry("", testFinder).password)
+        assertEquals("fooooo", makeEntry("fooooo\nbla\n").password)
+        assertEquals("fooooo", makeEntry("fooooo\nbla").password)
+        assertEquals("fooooo", makeEntry("fooooo\n").password)
+        assertEquals("fooooo", makeEntry("fooooo").password)
+        assertEquals("", makeEntry("\nblubb\n").password)
+        assertEquals("", makeEntry("\nblubb").password)
+        assertEquals("", makeEntry("\n").password)
+        assertEquals("", makeEntry("").password)
     }
 
     @Test fun testGetExtraContent() {
-        assertEquals("bla\n", PasswordEntry("fooooo\nbla\n", testFinder).extraContent)
-        assertEquals("bla", PasswordEntry("fooooo\nbla", testFinder).extraContent)
-        assertEquals("", PasswordEntry("fooooo\n", testFinder).extraContent)
-        assertEquals("", PasswordEntry("fooooo", testFinder).extraContent)
-        assertEquals("blubb\n", PasswordEntry("\nblubb\n", testFinder).extraContent)
-        assertEquals("blubb", PasswordEntry("\nblubb", testFinder).extraContent)
-        assertEquals("", PasswordEntry("\n", testFinder).extraContent)
-        assertEquals("", PasswordEntry("", testFinder).extraContent)
+        assertEquals("bla\n", makeEntry("fooooo\nbla\n").extraContent)
+        assertEquals("bla", makeEntry("fooooo\nbla").extraContent)
+        assertEquals("", makeEntry("fooooo\n").extraContent)
+        assertEquals("", makeEntry("fooooo").extraContent)
+        assertEquals("blubb\n", makeEntry("\nblubb\n").extraContent)
+        assertEquals("blubb", makeEntry("\nblubb").extraContent)
+        assertEquals("", makeEntry("\n").extraContent)
+        assertEquals("", makeEntry("").extraContent)
     }
 
     @Test fun testGetUsername() {
         for (field in PasswordEntry.USERNAME_FIELDS) {
-            assertEquals("username", PasswordEntry("\n$field username", testFinder).username)
-            assertEquals("username", PasswordEntry("\n${field.toUpperCase()} username", testFinder).username)
+            assertEquals("username", makeEntry("\n$field username").username)
+            assertEquals("username", makeEntry("\n${field.toUpperCase()} username").username)
         }
         assertEquals(
             "username",
-            PasswordEntry("secret\nextra\nlogin: username\ncontent\n", testFinder).username)
+            makeEntry("secret\nextra\nlogin: username\ncontent\n").username)
         assertEquals(
             "username",
-            PasswordEntry("\nextra\nusername: username\ncontent\n", testFinder).username)
+            makeEntry("\nextra\nusername: username\ncontent\n").username)
         assertEquals(
-            "username", PasswordEntry("\nUSERNaMe:  username\ncontent\n", testFinder).username)
-        assertEquals("username", PasswordEntry("\nlogin:    username", testFinder).username)
-        assertEquals("foo@example.com", PasswordEntry("\nemail: foo@example.com", testFinder).username)
-        assertEquals("username", PasswordEntry("\nidentity: username\nlogin: another_username", testFinder).username)
-        assertEquals("username", PasswordEntry("\nLOGiN:username", testFinder).username)
-        assertNull(PasswordEntry("secret\nextra\ncontent\n", testFinder).username)
+            "username", makeEntry("\nUSERNaMe:  username\ncontent\n").username)
+        assertEquals("username", makeEntry("\nlogin:    username").username)
+        assertEquals("foo@example.com", makeEntry("\nemail: foo@example.com").username)
+        assertEquals("username", makeEntry("\nidentity: username\nlogin: another_username").username)
+        assertEquals("username", makeEntry("\nLOGiN:username").username)
+        assertNull(makeEntry("secret\nextra\ncontent\n").username)
     }
 
     @Test fun testHasUsername() {
-        assertTrue(PasswordEntry("secret\nextra\nlogin: username\ncontent\n", testFinder).hasUsername())
-        assertFalse(PasswordEntry("secret\nextra\ncontent\n", testFinder).hasUsername())
-        assertFalse(PasswordEntry("secret\nlogin failed\n", testFinder).hasUsername())
-        assertFalse(PasswordEntry("\n", testFinder).hasUsername())
-        assertFalse(PasswordEntry("", testFinder).hasUsername())
+        assertTrue(makeEntry("secret\nextra\nlogin: username\ncontent\n").hasUsername())
+        assertFalse(makeEntry("secret\nextra\ncontent\n").hasUsername())
+        assertFalse(makeEntry("secret\nlogin failed\n").hasUsername())
+        assertFalse(makeEntry("\n").hasUsername())
+        assertFalse(makeEntry("").hasUsername())
+    }
+
+    companion object {
+        // This implementation just sends back defaults for now.
+        val testFinder = object : TotpFinder {
+            override fun findSecret(content: String): String? {
+                return ""
+            }
+
+            override fun findDigits(content: String): String {
+                return "6"
+            }
+
+            override fun findPeriod(content: String): Long {
+                return 30
+            }
+
+            override fun findAlgorithm(content: String): String {
+                return "HmacSHA1"
+            }
+        }
     }
 }
