2e6e4070b2516718703c1fd2da788ac3f5455c00
Harsh Shandilya <me@msfjarvis.dev>
2020-07-03 00:19:39 +0530

    release: script improvements
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-snapshot.sh
+++ release/deploy-snapshot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -ex
 
 export SSHDIR="$HOME/.ssh"
 mkdir -p "$SSHDIR"
@@ -6,8 +7,7 @@ echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
 chmod 600 "$SSHDIR/key"
 export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
 mkdir -p "$GITHUB_WORKSPACE/APS"
-cp -v "$GITHUB_WORKSPACE/app/build/outputs/apk/free/release/*.apk" "$GITHUB_WORKSPACE/APS"
-cp -v "$GITHUB_WORKSPACE/app/build/outputs/apk/nonFree/release/*.apk" "$GITHUB_WORKSPACE/APS"
+cp -v ./app/build/outputs/apk/free/release/*.apk "$GITHUB_WORKSPACE/APS/"
+cp -v ./app/build/outputs/apk/nonFree/release/*.apk "$GITHUB_WORKSPACE/APS/"
 cd "$GITHUB_WORKSPACE/APS"
-rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING" || exit 1
-exit 0
+rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING"
