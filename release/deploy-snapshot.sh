945828b4bb8dae2ebe3ac5d896a38c116a84c83f
Harsh Shandilya <me@msfjarvis.dev>
2020-07-02 19:42:36 +0530

    Deploy both free and non-free variants
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- release/deploy-snapshot.sh
+++ release/deploy-snapshot.sh
@@ -5,7 +5,9 @@ mkdir -p "$SSHDIR"
 echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
 chmod 600 "$SSHDIR/key"
 export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
-cd "$GITHUB_WORKSPACE/app/build/outputs/apk/free/release"
-rm output.json
+mkdir -p "$GITHUB_WORKSPACE/APS"
+cp -v "$GITHUB_WORKSPACE/app/build/outputs/apk/free/release/*.apk" "$GITHUB_WORKSPACE/APS"
+cp -v "$GITHUB_WORKSPACE/app/build/outputs/apk/nonFree/release/*.apk" "$GITHUB_WORKSPACE/APS"
+cd "$GITHUB_WORKSPACE/APS"
 rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING" || exit 1
 exit 0
