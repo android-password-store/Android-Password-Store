c1f5fc89656fad31a8e98bc3ab349efebec62d7f
Fabian Henneke <fabian@henneke.me>
2020-07-07 08:50:48 +0200

    Debounce node population and add manual triggers

--- app/src/main/java/com/zeapo/pwdstore/autofill/oreo/ChromeCompatFix.kt
+++ app/src/main/java/com/zeapo/pwdstore/autofill/oreo/ChromeCompatFix.kt
@@ -5,15 +5,46 @@
 package com.zeapo.pwdstore.autofill.oreo
 
 import android.accessibilityservice.AccessibilityService
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.view.accessibility.AccessibilityEvent
-import com.github.ajalt.timberkt.d
-import kotlin.system.measureTimeMillis
+import androidx.annotation.RequiresApi
+import com.github.ajalt.timberkt.v
 
 class ChromeCompatFix : AccessibilityService() {
 
+    private val handler = Handler(Looper.getMainLooper())
+    private val forceRootNodePopulation = Runnable {
+        val rootPackageName = rootInActiveWindow?.packageName.toString()
+        v { "$rootPackageName: forced root node population" }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        val time = measureTimeMillis { rootInActiveWindow }
-        d { "${event.packageName} (${AccessibilityEvent.eventTypeToString(event.eventType)}): $time ms" }
+        handler.removeCallbacks(forceRootNodePopulation)
+        when (event.eventType) {
+            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED, AccessibilityEvent.TYPE_ANNOUNCEMENT -> {
+                // WINDOW_STATE_CHANGED: Triggered on long press in a text field, replacement for
+                //                       the missing Autofill action menu item.
+                // ANNOUNCEMENT:         Triggered when a password field is selected.
+                //
+                // These events are triggered only by user actions and thus don't need to be handled
+                // with debounce. However, they only trigger Autofill popups on the *next* input
+                // field selected by the user.
+                forceRootNodePopulation.run()
+                v { "${event.packageName} (${AccessibilityEvent.eventTypeToString(event.eventType)}): forced root node population" }
+            }
+            AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
+                // WINDOW_CONTENT_CHANGED: Triggered whenever the page contents change.
+                //
+                // This event is triggered many times during page load, which makes a debounce
+                // necessary to prevent huge performance regressions in Chrome. However, it is the
+                // only event that reliably runs before the user selects a text field.
+                handler.postDelayed(forceRootNodePopulation, 300)
+                v { "${event.packageName} (${AccessibilityEvent.eventTypeToString(event.eventType)}): debounced root node population" }
+            }
+        }
     }
 
     override fun onInterrupt() {}
