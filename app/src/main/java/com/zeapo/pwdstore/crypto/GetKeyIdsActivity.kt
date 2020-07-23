05f68da853e5c86cb7d4c481944956fc1592e929
Fabian Henneke <fabian@henneke.me>
2020-07-23 16:14:26 +0200

    Refactor and remove unused preferences

--- app/src/main/java/com/zeapo/pwdstore/crypto/GetKeyIdsActivity.kt
+++ app/src/main/java/com/zeapo/pwdstore/crypto/GetKeyIdsActivity.kt
@@ -9,10 +9,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
-import androidx.core.content.edit
 import androidx.lifecycle.lifecycleScope
 import com.github.ajalt.timberkt.e
-import com.zeapo.pwdstore.utils.PreferenceKeys
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.msfjarvis.openpgpktx.util.OpenPgpApi
@@ -22,19 +20,12 @@ import org.openintents.openpgp.IOpenPgpService2
 class GetKeyIdsActivity : BasePgpActivity() {
 
     private val userInteractionRequiredResult = registerForActivityResult(StartIntentSenderForResult()) { result ->
-        if (result.data == null) {
-            setResult(RESULT_CANCELED, null)
+        if (result.data == null || result.resultCode == RESULT_CANCELED) {
+            setResult(RESULT_CANCELED, result.data)
             finish()
             return@registerForActivityResult
         }
-
-        when (result.resultCode) {
-            RESULT_OK -> getKeyIds(result.data)
-            RESULT_CANCELED -> {
-                setResult(RESULT_CANCELED, result.data)
-                finish()
-            }
-        }
+        getKeyIds(result.data!!)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,23 +45,18 @@ class GetKeyIdsActivity : BasePgpActivity() {
     /**
      * Get the Key ids from OpenKeychain
      */
-    private fun getKeyIds(receivedIntent: Intent? = null) {
-        val data = receivedIntent ?: Intent()
+    private fun getKeyIds(data: Intent = Intent()) {
         data.action = OpenPgpApi.ACTION_GET_KEY_IDS
         lifecycleScope.launch(Dispatchers.IO) {
             api?.executeApiAsync(data, null, null) { result ->
                 when (result?.getIntExtra(OpenPgpApi.RESULT_CODE, OpenPgpApi.RESULT_CODE_ERROR)) {
                     OpenPgpApi.RESULT_CODE_SUCCESS -> {
                         try {
-                            val ids = result.getLongArrayExtra(OpenPgpApi.RESULT_KEY_IDS)
-                                ?: LongArray(0)
-
-                            val keys = ids.map { it.toString() }.toSet()
-                            // use Long
-                            settings.edit { putStringSet(PreferenceKeys.OPENPGP_KEY_IDS_SET, keys) }
-                            val keyResult = Intent()
-                            if (ids.isNotEmpty()) {
-                                keyResult.putExtra(OpenPgpApi.EXTRA_KEY_ID, OpenPgpUtils.convertKeyIdToHex(ids.elementAt(0)))
+                            val id = result.getLongArrayExtra(OpenPgpApi.RESULT_KEY_IDS)?.firstOrNull()
+                            val keyResult = Intent().apply {
+                                if (id != null) {
+                                    putExtra(OpenPgpApi.EXTRA_KEY_ID, OpenPgpUtils.convertKeyIdToHex(id))
+                                }
                             }
                             setResult(RESULT_OK, keyResult)
                             finish()
