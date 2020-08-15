android-password-store/Android-Password-Store

commit 3d8cea5966002a9f383f227be8d31c9265c108b4

Improve permission handling logic (#732)
* Improve permission handling logic

Ensure we always ask for storage permissions when required

* Refactor storage permission checks and invert return value

* PasswordStore: improve permission grant flow

* strings: slightly reword permission grant request message

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

Co-authored-by: Fabian Henneke <fabian@henneke.me>
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/PasswordStore.kt
@@ -12,7 +12,6 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.content.pm.ShortcutInfo.Builder
 import android.content.pm.ShortcutManager
-import android.graphics.Color
 import android.graphics.drawable.Icon
 import android.net.Uri
 import android.os.Build
@@ -180,34 +179,7 @@ class PasswordStore : AppCompatActivity() {
         super.onResume()
         // do not attempt to checkLocalRepository() if no storage permission: immediate crash
         if (settings.getBoolean("git_external", false)) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    val snack = Snackbar.make(
-                                    findViewById(R.id.main_layout),
-                                    getString(R.string.access_sdcard_text),
-                                    Snackbar.LENGTH_INDEFINITE)
-                            .setAction(R.string.dialog_ok) {
-                                ActivityCompat.requestPermissions(
-                                        activity,
-                                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                                        REQUEST_EXTERNAL_STORAGE)
-                            }
-                    snack.show()
-                    val view = snack.view
-                    val tv: AppCompatTextView = view.findViewById(com.google.android.material.R.id.snackbar_text)
-                    tv.setTextColor(Color.WHITE)
-                    tv.maxLines = 10
-                } else {
-                    // No explanation needed, we can request the permission.
-                    ActivityCompat.requestPermissions(
-                            activity,
-                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                            REQUEST_EXTERNAL_STORAGE)
-                }
-            } else {
-                checkLocalRepository()
-            }
+            hasRequiredStoragePermissions(true)
         } else {
             checkLocalRepository()
         }
@@ -389,8 +361,12 @@ class PasswordStore : AppCompatActivity() {
     }
 
     private fun initializeRepositoryInfo() {
+        val externalRepo = settings.getBoolean("git_external", false)
         val externalRepoPath = settings.getString("git_external_repo", null)
-        if (settings.getBoolean("git_external", false) && externalRepoPath != null) {
+        if (externalRepo && !hasRequiredStoragePermissions()) {
+            return
+        }
+        if (externalRepo && externalRepoPath != null) {
             val dir = File(externalRepoPath)
             if (dir.exists() && dir.isDirectory &&
                     getPasswords(dir, getRepositoryDirectory(this), sortOrder).isNotEmpty()) {
@@ -411,6 +387,36 @@ class PasswordStore : AppCompatActivity() {
                     .show()
         }
         createRepository()
+    }
+
+    /**
+     * Validates if storage permission is granted, and requests for it if not. The return value
+     * is true if the permission has been granted.
+     */
+    private fun hasRequiredStoragePermissions(checkLocalRepo: Boolean = false): Boolean {
+        return if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(
+                    findViewById(R.id.main_layout),
+                    getString(R.string.access_sdcard_text),
+                    Snackbar.LENGTH_INDEFINITE
+            ).run {
+                setAction(getString(R.string.snackbar_action_grant)) {
+                    ActivityCompat.requestPermissions(
+                            activity,
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            REQUEST_EXTERNAL_STORAGE
+                    )
+                    dismiss()
+                }
+                show()
+            }
+            false
+        } else {
+            if (checkLocalRepo)
+                checkLocalRepository()
+            true
+        }
     }
 
     private fun checkLocalRepository() {
@@ -631,7 +637,7 @@ class PasswordStore : AppCompatActivity() {
         get() = plist?.currentDir ?: getRepositoryDirectory(applicationContext)
 
     private fun commitChange(message: String) {
-        Companion.commitChange(activity, message)
+        commitChange(activity, message)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {

app/src/main/java/com/zeapo/pwdstore/UserPreference.kt
@@ -461,6 +461,12 @@ class UserPreference : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         when (intent?.getStringExtra("operation")) {

app/src/main/res/values/strings.xml
@@ -333,7 +333,7 @@
     <string name="biometric_auth_summary">When enabled, Password Store will prompt you for your fingerprint when launching the app</string>
     <string name="biometric_auth_summary_error">Fingerprint hardware not accessible or missing</string>
     <string name="ssh_openkeystore_clear_keyid">Clear remembered OpenKeystore SSH Key ID</string>
-    <string name="access_sdcard_text">The store is on the sdcard but the app does not have permission to access it. Please give permission.</string>
+    <string name="access_sdcard_text">The store location is in your SD Card or Internal storage, but the app does not have permission to access it.</string>
     <string name="your_public_key">Your public key</string>
     <string name="error_generate_ssh_key">Error while trying to generate the ssh-key</string>
     <string name="pref_show_hidden_title">Show hidden folders</string>
@@ -364,4 +364,5 @@
     <string name="bottom_sheet_create_new_password">Create new password</string>
     <string name="autofill_onboarding_dialog_title">New, revamped Autofill!</string>
     <string name="autofill_onboarding_dialog_message">In this release, Autofill support has been massively improved with advanced features like anti-phishing protection and enhanced reliability. If you have been holding out on using it because of the shortcomings on the previous version, you\'ll likely love the new iteration. Give it a shot!</string>
+    <string name="snackbar_action_grant">Grant</string>
 </resources>

app/src/main/res/values-ru/strings.xml
@@ -303,7 +303,6 @@
     <string name="biometric_auth_summary">Когда ключено, Password Store будет запрашивать ваш опечаток пальца при каждом запуске приложения</string>
     <string name="biometric_auth_summary_error">Сенсор отпечатка пальца не доступен или отсутствует</string>
     <string name="ssh_openkeystore_clear_keyid">Очистить сохраненный SSH Key идентификатор OpenKystortore</string>
-    <string name="access_sdcard_text">Хранилище находится на sd-карте, но приложение не имеет прав доступа к ней. Пожалуйста, дайте приложению необходимые разрешения.</string>
     <string name="your_public_key">Ваш публичный ключ</string>
     <string name="error_generate_ssh_key">Возникла ошибка при попытке генерации ssh ключа</string>
     <string name="pref_show_hidden_title">Показать скрытые папки</string>

commit 2ffd1abb279bb0e1a8895ef26b805a1a1651516c

Fix external storage UX (#1022)
* build: update to Kotlin 1.4

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* UserPreference: finish if directory selection was triggered from an intent

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* PasswordStore: switch permission request to ActivityResultContracts

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* PasswordStore: fix activity reference

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* GitOperationActivity: make invalid values more obvious

Would have caught this issue much sooner if I had just done this

Fixes: 3d8cea596600 ("Improve permission handling logic (#732)")
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Assorted collection of hackery to make external storage use palatable

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Update changelog

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/PasswordStore.kt
@@ -19,13 +19,13 @@ import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MenuItem.OnActionExpandListener
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.edit
 import androidx.core.content.getSystemService
@@ -127,7 +127,13 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
                     return@registerForActivityResult
                 }
             }
-            val intent = Intent(activity, GitOperationActivity::class.java)
+            checkPermissionsAndCloneAction.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    private val checkPermissionsAndCloneAction = registerForActivityResult(RequestPermission()) { granted ->
+        if (granted) {
+            val intent = Intent(activity, GitServerConfigActivity::class.java)
             intent.putExtra(BaseGitActivity.REQUEST_ARG_OP, BaseGitActivity.REQUEST_CLONE)
             cloneAction.launch(intent)
         }
@@ -220,14 +226,13 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
         }
     }
 
-    public override fun onStart() {
+    override fun onStart() {
         super.onStart()
         refreshPasswordList()
     }
 
-    public override fun onResume() {
+    override fun onResume() {
         super.onResume()
-        // do not attempt to checkLocalRepository() if no storage permission: immediate crash
         if (settings.getBoolean(PreferenceKeys.GIT_EXTERNAL, false)) {
             hasRequiredStoragePermissions(true)
         } else {
@@ -236,16 +241,6 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
         if (settings.getBoolean(PreferenceKeys.SEARCH_ON_START, false) && ::searchItem.isInitialized) {
             if (!searchItem.isActionViewExpanded) {
                 searchItem.expandActionView()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        // If request is cancelled, the result arrays are empty.
-        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                checkLocalRepository()
             }
         }
     }
@@ -423,19 +418,18 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
      * is true if the permission has been granted.
      */
     private fun hasRequiredStoragePermissions(checkLocalRepo: Boolean = false): Boolean {
+        val cloning = supportFragmentManager.findFragmentByTag("ToCloneOrNot") != null
         return if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED) {
+            != PackageManager.PERMISSION_GRANTED && !cloning) {
             Snackbar.make(
                 findViewById(R.id.main_layout),
                 getString(R.string.access_sdcard_text),
                 Snackbar.LENGTH_INDEFINITE
             ).run {
                 setAction(getString(R.string.snackbar_action_grant)) {
-                    ActivityCompat.requestPermissions(
-                        activity,
-                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                        REQUEST_EXTERNAL_STORAGE
-                    )
+                    registerForActivityResult(RequestPermission()) { granted ->
+                        if (granted) checkLocalRepository()
+                    }.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     dismiss()
                 }
                 show()
@@ -491,7 +485,7 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
             supportActionBar!!.hide()
             supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
             supportFragmentManager.commit {
-                replace(R.id.main_layout, ToCloneOrNot())
+                replace(R.id.main_layout, ToCloneOrNot(), "ToCloneOrNot")
             }
         }
     }
@@ -903,7 +897,6 @@ class PasswordStore : AppCompatActivity(R.layout.activity_pwdstore) {
         const val REQUEST_ARG_PATH = "PATH"
         const val CLONE_REPO_BUTTON = 401
         const val NEW_REPO_BUTTON = 402
-        private const val REQUEST_EXTERNAL_STORAGE = 50
         private fun isPrintable(c: Char): Boolean {
             val block = UnicodeBlock.of(c)
             return (!Character.isISOControl(c) &&

app/src/main/java/com/zeapo/pwdstore/UserPreference.kt
@@ -464,7 +464,7 @@ class UserPreference : AppCompatActivity() {
         when (intent?.getStringExtra("operation")) {
             "get_ssh_key" -> getSshKey()
             "make_ssh_key" -> makeSshKey(false)
-            "git_external" -> selectExternalGitRepository()
+            "git_external" -> selectExternalGitRepository(fromIntent = true)
         }
         prefsFragment = PrefsFragment()
 
@@ -477,7 +477,7 @@ class UserPreference : AppCompatActivity() {
     }
 
     @Suppress("Deprecation") // for Environment.getExternalStorageDirectory()
-    fun selectExternalGitRepository() {
+    fun selectExternalGitRepository(fromIntent: Boolean = false) {
         MaterialAlertDialogBuilder(this)
             .setTitle(this.resources.getString(R.string.external_repository_dialog_title))
             .setMessage(this.resources.getString(R.string.external_repository_dialog_text))
@@ -506,6 +506,10 @@ class UserPreference : AppCompatActivity() {
                             .show()
                     }
                     prefs.edit { putString(PreferenceKeys.GIT_EXTERNAL_REPO, repoPath) }
+                    if (fromIntent) {
+                        setResult(RESULT_OK)
+                        finish()
+                    }
 
                 }.launch(null)
             }

app/src/main/java/com/zeapo/pwdstore/git/GitOperationActivity.kt
@@ -20,13 +20,12 @@ open class GitOperationActivity : BaseGitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        when (intent.extras?.getInt(REQUEST_ARG_OP)) {
+        when (val reqCode = intent.extras?.getInt(REQUEST_ARG_OP)) {
             REQUEST_PULL -> lifecycleScope.launch { syncRepository(REQUEST_PULL) }
             REQUEST_PUSH -> lifecycleScope.launch { syncRepository(REQUEST_PUSH) }
             REQUEST_SYNC -> lifecycleScope.launch { syncRepository(REQUEST_SYNC) }
             else -> {
-                setResult(RESULT_CANCELED)
-                finish()
+                throw IllegalArgumentException("Invalid request code: $reqCode")
             }
         }
     }

app/src/main/java/com/zeapo/pwdstore/git/GitServerConfigActivity.kt
@@ -17,9 +17,12 @@ import com.zeapo.pwdstore.git.config.ConnectionMode
 import com.zeapo.pwdstore.git.config.GitSettings
 import com.zeapo.pwdstore.git.config.Protocol
 import com.zeapo.pwdstore.utils.PasswordRepository
+import com.zeapo.pwdstore.utils.snackbar
 import com.zeapo.pwdstore.utils.viewBinding
 import java.io.IOException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Activity that encompasses both the initial clone as well as editing the server config for future
@@ -125,8 +128,14 @@ class GitServerConfigActivity : BaseGitActivity() {
                 .setCancelable(false)
                 .setPositiveButton(R.string.dialog_delete) { dialog, _ ->
                     try {
-                        localDir.deleteRecursively()
-                        lifecycleScope.launch { launchGitOperation(REQUEST_CLONE) }
+                        lifecycleScope.launch {
+                            val snackbar = snackbar(message = getString(R.string.delete_directory_progress_text), length = Snackbar.LENGTH_INDEFINITE)
+                            withContext(Dispatchers.IO) {
+                                localDir.deleteRecursively()
+                            }
+                            snackbar.dismiss()
+                            launchGitOperation(REQUEST_CLONE)
+                        }
                     } catch (e: IOException) {
                         // TODO Handle the exception correctly if we are unable to delete the directory...
                         e.printStackTrace()

app/src/main/java/com/zeapo/pwdstore/utils/PasswordRepository.kt
@@ -169,7 +169,7 @@ open class PasswordRepository protected constructor() {
             val dir = getRepositoryDirectory()
             // uninitialize the repo if the dir does not exist or is absolutely empty
             settings.edit {
-                if (!dir.exists() || !dir.isDirectory || dir.listFiles()!!.isEmpty()) {
+                if (!dir.exists() || !dir.isDirectory || dir.listFiles()?.isEmpty() == true) {
                     putBoolean(PreferenceKeys.REPOSITORY_INITIALIZED, false)
                 } else {
                     putBoolean(PreferenceKeys.REPOSITORY_INITIALIZED, true)

app/src/main/res/values/strings.xml
@@ -27,6 +27,7 @@
         <item quantity="one">Are you sure you want to delete the password?</item>
         <item quantity="other">Are you sure you want to delete %d passwords?</item>
     </plurals>
+    <string name="delete_directory_progress_text">Deleting…</string>
     <string name="move">Move</string>
     <string name="edit">Edit</string>
     <string name="delete">Delete</string>