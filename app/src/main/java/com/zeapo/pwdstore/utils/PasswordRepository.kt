fb66a5c87b5876c36db9acf2d736dd20f0f06ccc
Alex Molinares <amolinares19@gmail.com>
2020-08-19 18:40:06 +0200

    added new sharedPreferences file to manage recent password history

--- app/src/main/java/com/zeapo/pwdstore/utils/PasswordRepository.kt
+++ app/src/main/java/com/zeapo/pwdstore/utils/PasswordRepository.kt
@@ -32,8 +32,9 @@ open class PasswordRepository protected constructor() {
         }),
 
         RECENTLY_USED(Comparator { p1: PasswordItem, p2: PasswordItem ->
-            val timeP1 = settings.getString(p1.file.absolutePath)
-            val timeP2 = settings.getString(p2.file.absolutePath)
+            val recentHistory = Application.instance.getSharedPreferences("recent_password_history", android.content.Context.MODE_PRIVATE)
+            val timeP1 = recentHistory.getString(p1.file.absolutePath)
+            val timeP2 = recentHistory.getString(p2.file.absolutePath)
             when {
                 timeP1 != null && timeP2 != null -> timeP2.compareTo(timeP1)
                 timeP1 != null && timeP2 == null -> return@Comparator -1
