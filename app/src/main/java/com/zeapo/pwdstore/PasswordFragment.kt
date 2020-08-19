d82e548ba994170a782db227dfc2531123e0672d
Diogenes Molinares <amolinares19@gmail.com>
2020-08-19 20:01:14 +0200

    Update app/src/main/java/com/zeapo/pwdstore/PasswordFragment.kt
    Co-authored-by: Fabian Henneke <FabianHenneke@users.noreply.github.com>

--- app/src/main/java/com/zeapo/pwdstore/PasswordFragment.kt
+++ app/src/main/java/com/zeapo/pwdstore/PasswordFragment.kt
@@ -247,7 +247,7 @@ class PasswordFragment : Fragment(R.layout.password_recycler_view) {
                 override fun onFragmentInteraction(item: PasswordItem) {
                     if (settings.getString(PreferenceKeys.SORT_ORDER) == PasswordRepository.PasswordSortOrder.RECENTLY_USED.name) {
                         //save the time when password was used
-                        val preferences = context.getSharedPreferences("recent_password_history", Context.MODE_PRIVATE )
+                        val preferences = context.getSharedPreferences("recent_password_history", Context.MODE_PRIVATE)
                         val editor = preferences.edit()
                         editor.putString(item.file.absolutePath, System.currentTimeMillis().toString())
                         editor.apply()
