android-password-store/Android-Password-Store

commit 80e43e5675bf55204dd4fcb1adc02f7edcd62516

Update dependencies (#1039)
* build: uprev dependencies

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Address deprecation warnings

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Update autofill, appcompat and core

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/utils/BiometricAuthenticator.kt
@@ -63,10 +63,9 @@ object BiometricAuthenticator {
         val biometricPrompt = BiometricPrompt(activity, { handler.post(it) }, authCallback)
         val promptInfo = BiometricPrompt.PromptInfo.Builder()
             .setTitle(activity.getString(dialogTitleRes))
-            .setDeviceCredentialAllowed(true)
             .build()
-        if (BiometricManager.from(activity).canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS ||
-            activity.getSystemService<KeyguardManager>()?.isDeviceSecure == true) {
+        if (BiometricManager.from(activity).canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+            == BiometricManager.BIOMETRIC_SUCCESS || activity.getSystemService<KeyguardManager>()?.isDeviceSecure == true) {
             biometricPrompt.authenticate(promptInfo)
         } else {
             callback(Result.HardwareUnavailableOrDisabled)

app/src/main/java/com/zeapo/pwdstore/UserPreference.kt
@@ -279,7 +279,8 @@ class UserPreference : AppCompatActivity() {
             findPreference<CheckBoxPreference>(PreferenceKeys.ENABLE_DEBUG_LOGGING)?.isVisible = !BuildConfig.ENABLE_DEBUG_FEATURES
 
             findPreference<CheckBoxPreference>(PreferenceKeys.BIOMETRIC_AUTH)?.apply {
-                val isFingerprintSupported = BiometricManager.from(requireContext()).canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS
+                val isFingerprintSupported = BiometricManager.from(requireContext())
+                    .canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS
                 if (!isFingerprintSupported) {
                     isEnabled = false
                     isChecked = false

buildSrc/src/main/java/Dependencies.kt
@@ -23,18 +23,20 @@ object Dependencies {
 
     object AndroidX {
 
-        const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-alpha07"
+        private const val lifecycleVersion = "2.3.0-alpha07"
+
+        const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-alpha08"
         const val annotation = "androidx.annotation:annotation:1.2.0-alpha01"
-        const val autofill = "androidx.autofill:autofill:1.1.0-alpha01"
-        const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha01"
-        const val biometric = "androidx.biometric:biometric:1.1.0-alpha01"
+        const val autofill = "androidx.autofill:autofill:1.1.0-alpha02"
+        const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha02"
+        const val biometric = "androidx.biometric:biometric:1.1.0-alpha02"
         const val constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.0-rc1"
-        const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha01"
+        const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha02"
         const val documentfile = "androidx.documentfile:documentfile:1.0.1"
-        const val fragment_ktx = "androidx.fragment:fragment-ktx:1.3.0-alpha07"
-        const val lifecycle_common = "androidx.lifecycle:lifecycle-common-java8:2.3.0-alpha06"
-        const val lifecycle_livedata_ktx = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.0-alpha06"
-        const val lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-alpha06"
+        const val fragment_ktx = "androidx.fragment:fragment-ktx:1.3.0-alpha08"
+        const val lifecycle_common = "androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion"
+        const val lifecycle_livedata_ktx = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion"
+        const val lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
         const val material = "com.google.android.material:material:1.3.0-alpha02"
         const val preference = "androidx.preference:preference:1.1.1"
         const val recycler_view = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
@@ -62,7 +64,7 @@ object Dependencies {
         const val ssh_auth = "org.sufficientlysecure:sshauthentication-api:1.0"
         const val timber = "com.jakewharton.timber:timber:4.7.1"
         const val timberkt = "com.github.ajalt:timberkt:1.5.1"
-        const val whatthestack = "com.github.haroldadmin:WhatTheStack:0.0.4"
+        const val whatthestack = "com.github.haroldadmin:WhatTheStack:0.0.5"
     }
 
     object NonFree {

commit 9afa486223fcc0c6bd991ac3159324438b785b8d

BiometricAuthenticator: fix multiple regressions from AndroidX update
Fixes: 80e43e5675bf ("Update dependencies (#1039)")
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/utils/BiometricAuthenticator.kt
@@ -9,6 +9,7 @@ import android.os.Handler
 import androidx.annotation.StringRes
 import androidx.biometric.BiometricConstants
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.getSystemService
 import androidx.fragment.app.FragmentActivity
@@ -60,13 +61,15 @@ object BiometricAuthenticator {
                 callback(Result.Success(result.cryptoObject))
             }
         }
-        val biometricPrompt = BiometricPrompt(activity, { handler.post(it) }, authCallback)
-        val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle(activity.getString(dialogTitleRes))
-            .build()
-        if (BiometricManager.from(activity).canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL)
-            == BiometricManager.BIOMETRIC_SUCCESS || activity.getSystemService<KeyguardManager>()?.isDeviceSecure == true) {
-            biometricPrompt.authenticate(promptInfo)
+        val validAuthenticators = Authenticators.DEVICE_CREDENTIAL or Authenticators.BIOMETRIC_STRONG
+        val canAuth = BiometricManager.from(activity).canAuthenticate(validAuthenticators) == BiometricManager.BIOMETRIC_SUCCESS
+        val deviceHasKeyguard = activity.getSystemService<KeyguardManager>()?.isDeviceSecure == true
+        if (canAuth || deviceHasKeyguard) {
+            val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle(activity.getString(dialogTitleRes))
+                .setAllowedAuthenticators(validAuthenticators)
+                .build()
+            BiometricPrompt(activity, { handler.post(it) }, authCallback).authenticate(promptInfo)
         } else {
             callback(Result.HardwareUnavailableOrDisabled)
         }