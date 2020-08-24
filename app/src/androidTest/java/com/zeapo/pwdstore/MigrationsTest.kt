fef01ef29cafd68dc9288eab97abcdfa59417365
Fabian Henneke <fabian@henneke.me>
2020-08-24 11:31:17 +0200

    Fix instrumentation tests

--- app/src/androidTest/java/com/zeapo/pwdstore/MigrationsTest.kt
+++ app/src/androidTest/java/com/zeapo/pwdstore/MigrationsTest.kt
@@ -8,6 +8,8 @@ package com.zeapo.pwdstore
 
 import android.content.Context
 import androidx.core.content.edit
+import com.zeapo.pwdstore.git.config.ConnectionMode
+import com.zeapo.pwdstore.git.config.Protocol
 import com.zeapo.pwdstore.utils.PreferenceKeys
 import com.zeapo.pwdstore.utils.getString
 import com.zeapo.pwdstore.utils.sharedPrefs
@@ -33,7 +35,8 @@ class MigrationsTest {
             putString(PreferenceKeys.GIT_REMOTE_USERNAME, "msfjarvis")
             putString(PreferenceKeys.GIT_REMOTE_LOCATION, "/mnt/disk3/pass-repo")
             putString(PreferenceKeys.GIT_REMOTE_SERVER, "192.168.0.102")
-            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, "ssh://")
+            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, Protocol.Ssh.pref)
+            putString(PreferenceKeys.GIT_REMOTE_AUTH, ConnectionMode.Password.pref)
         }
         runMigrations(context)
         checkOldKeysAreRemoved(context)
@@ -51,7 +54,8 @@ class MigrationsTest {
             putString(PreferenceKeys.GIT_REMOTE_USERNAME, "msfjarvis")
             putString(PreferenceKeys.GIT_REMOTE_LOCATION, "/mnt/disk3/pass-repo")
             putString(PreferenceKeys.GIT_REMOTE_SERVER, "192.168.0.102")
-            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, "ssh://")
+            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, Protocol.Ssh.pref)
+            putString(PreferenceKeys.GIT_REMOTE_AUTH, ConnectionMode.SshKey.pref)
         }
         runMigrations(context)
         checkOldKeysAreRemoved(context)
@@ -69,7 +73,8 @@ class MigrationsTest {
             putString(PreferenceKeys.GIT_REMOTE_USERNAME, "msfjarvis")
             putString(PreferenceKeys.GIT_REMOTE_LOCATION, "Android-Password-Store/pass-test")
             putString(PreferenceKeys.GIT_REMOTE_SERVER, "github.com")
-            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, "https://")
+            putString(PreferenceKeys.GIT_REMOTE_PROTOCOL, Protocol.Https.pref)
+            putString(PreferenceKeys.GIT_REMOTE_AUTH, ConnectionMode.None.pref)
         }
         runMigrations(context)
         checkOldKeysAreRemoved(context)
