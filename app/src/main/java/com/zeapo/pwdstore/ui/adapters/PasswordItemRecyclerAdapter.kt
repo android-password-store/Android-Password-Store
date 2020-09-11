android-password-store/Android-Password-Store

commit 1ce3ef4ea3c55d4a6c4a79f8a6cca58387b240df

Expand show hidden folders to also cover files (#1059)
* PasswordItem: only strip .gpg suffixes

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Add preference key and migration for showing all hidden contents

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Allow showing both hidden files and directories

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Add tests for hidden folder setting migration

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Add changelog entry

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Slightly improve migration logic

Skip migration if old key is not found and always delete the previous key even if its set to false.

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Tweak wording

Suggested-by: Fabian Henneke <fabian@henneke.me>
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

* Assert previous key's removal in tests

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/ui/adapters/PasswordItemRecyclerAdapter.kt
@@ -50,7 +50,7 @@ open class PasswordItemRecyclerAdapter :
 
         fun bind(item: PasswordItem) {
             val settings = itemView.context.sharedPrefs
-            val showHidden = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, false)
+            val showHidden = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false)
             val parentPath = item.fullPathToParent.replace("(^/)|(/$)".toRegex(), "")
             val source = if (parentPath.isNotEmpty()) {
                 "$parentPath\n$item"
@@ -62,8 +62,8 @@ open class PasswordItemRecyclerAdapter :
             name.text = spannable
             if (item.type == PasswordItem.TYPE_CATEGORY) {
                 folderIndicator.visibility = View.VISIBLE
-                val children = item.file.listFiles { pathname ->
-                    !(!showHidden && (pathname.isDirectory && pathname.isHidden))
+                val children = with(item.file) {
+                    if (showHidden) listFiles() else listFiles { pathname -> pathname.isDirectory && !pathname.isHidden }
                 } ?: emptyArray<File>()
                 val count = children.size
                 childCount.visibility = if (count > 0) View.VISIBLE else View.GONE

app/src/androidTest/java/com/zeapo/pwdstore/MigrationsTest.kt
@@ -4,6 +4,7 @@
  */
 
 @file:Suppress("DEPRECATION")
+
 package com.zeapo.pwdstore
 
 import android.content.Context
@@ -13,9 +14,9 @@ import com.zeapo.pwdstore.git.config.Protocol
 import com.zeapo.pwdstore.utils.PreferenceKeys
 import com.zeapo.pwdstore.utils.getString
 import com.zeapo.pwdstore.utils.sharedPrefs
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class MigrationsTest {
 
@@ -83,5 +84,26 @@ class MigrationsTest {
             context.sharedPrefs.getString(PreferenceKeys.GIT_REMOTE_URL),
             "https://github.com/Android-Password-Store/pass-test"
         )
+    }
+
+    @Test
+    fun verifyHiddenFoldersMigrationIfDisabled() {
+        val context = Application.instance.applicationContext
+        context.sharedPrefs.edit { clear() }
+        runMigrations(context)
+        assertEquals(true, context.sharedPrefs.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, true))
+        assertEquals(false, context.sharedPrefs.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false))
+    }
+
+    @Test
+    fun verifyHiddenFoldersMigrationIfEnabled() {
+        val context = Application.instance.applicationContext
+        context.sharedPrefs.edit {
+            clear()
+            putBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, true)
+        }
+        runMigrations(context)
+        assertEquals(false, context.sharedPrefs.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, false))
+        assertEquals(true, context.sharedPrefs.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false))
     }
 }

app/src/main/java/com/zeapo/pwdstore/Migrations.kt
@@ -19,6 +19,7 @@ import java.net.URI
 
 fun runMigrations(context: Context) {
     migrateToGitUrlBasedConfig(context)
+    migrateToHideAll(context)
 }
 
 private fun migrateToGitUrlBasedConfig(context: Context) {
@@ -82,5 +83,14 @@ private fun migrateToGitUrlBasedConfig(context: Context) {
             newUrl = url,
             newBranch = GitSettings.branch) != GitSettings.UpdateConnectionSettingsResult.Valid) {
         e { "Failed to migrate to URL-based Git config, generated URL is invalid" }
+    }
+}
+
+private fun migrateToHideAll(context: Context) {
+    context.sharedPrefs.all[PreferenceKeys.SHOW_HIDDEN_FOLDERS] ?: return
+    val isHidden = context.sharedPrefs.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, false)
+    context.sharedPrefs.edit {
+        remove(PreferenceKeys.SHOW_HIDDEN_FOLDERS)
+        putBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, isHidden)
     }
 }

app/src/main/java/com/zeapo/pwdstore/SearchableRepositoryViewModel.kt
@@ -94,7 +94,7 @@ private fun PasswordItem.Companion.makeComparator(
         PasswordRepository.PasswordSortOrder.FOLDER_FIRST -> compareBy { it.type }
         // In order to let INDEPENDENT not distinguish between items based on their type, we simply
         // declare them all equal at this stage.
-        PasswordRepository.PasswordSortOrder.INDEPENDENT -> Comparator<PasswordItem> { _, _ -> 0 }
+        PasswordRepository.PasswordSortOrder.INDEPENDENT -> Comparator { _, _ -> 0 }
         PasswordRepository.PasswordSortOrder.FILE_FIRST -> compareByDescending { it.type }
         PasswordRepository.PasswordSortOrder.RECENTLY_USED -> PasswordRepository.PasswordSortOrder.RECENTLY_USED.comparator
     }
@@ -140,8 +140,8 @@ class SearchableRepositoryViewModel(application: Application) : AndroidViewModel
     private val root
         get() = PasswordRepository.getRepositoryDirectory()
     private val settings by lazy { application.sharedPrefs }
-    private val showHiddenDirs
-        get() = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, false)
+    private val showHiddenContents
+        get() = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false)
     private val defaultSearchMode
         get() = if (settings.getBoolean(PreferenceKeys.FILTER_RECURSIVELY, true)) {
             SearchMode.RecursivelyInSubdirectories
@@ -254,8 +254,9 @@ class SearchableRepositoryViewModel(application: Application) : AndroidViewModel
         }.asLiveData(Dispatchers.IO)
 
     private fun shouldTake(file: File) = with(file) {
+        if (showHiddenContents) return true
         if (isDirectory) {
-            !isHidden || showHiddenDirs
+            !isHidden
         } else {
             !isHidden && file.extension == "gpg"
         }

app/src/main/java/com/zeapo/pwdstore/utils/PasswordItem.kt
@@ -33,7 +33,7 @@ data class PasswordItem(
     }
 
     override fun toString(): String {
-        return name.replace(".gpg", "")
+        return name.replace("\\.gpg$".toRegex(), "")
     }
 
     override fun hashCode(): Int {

app/src/main/java/com/zeapo/pwdstore/utils/PasswordRepository.kt
@@ -226,12 +226,10 @@ open class PasswordRepository protected constructor() {
             // We need to recover the passwords then parse the files
             val passList = getFilesList(path).also { it.sortBy { f -> f.name } }
             val passwordList = ArrayList<PasswordItem>()
-            val showHiddenDirs = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_FOLDERS, false)
+            val showHidden = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false)
 
             if (passList.size == 0) return passwordList
-            if (showHiddenDirs) {
-                passList.filter { !(it.isFile && it.isHidden) }.toCollection(passList.apply { clear() })
-            } else {
+            if (!showHidden) {
                 passList.filter { !it.isHidden }.toCollection(passList.apply { clear() })
             }
             passList.forEach { file ->

app/src/main/java/com/zeapo/pwdstore/utils/PreferenceKeys.kt
@@ -28,16 +28,21 @@ object PreferenceKeys {
     const val GIT_EXTERNAL = "git_external"
     const val GIT_EXTERNAL_REPO = "git_external_repo"
     const val GIT_REMOTE_AUTH = "git_remote_auth"
+
     @Deprecated("Use GIT_REMOTE_URL instead")
     const val GIT_REMOTE_LOCATION = "git_remote_location"
+
     @Deprecated("Use GIT_REMOTE_URL instead")
     const val GIT_REMOTE_PORT = "git_remote_port"
+
     @Deprecated("Use GIT_REMOTE_URL instead")
     const val GIT_REMOTE_PROTOCOL = "git_remote_protocol"
     const val GIT_DELETE_REPO = "git_delete_repo"
+
     @Deprecated("Use GIT_REMOTE_URL instead")
     const val GIT_REMOTE_SERVER = "git_remote_server"
     const val GIT_REMOTE_URL = "git_remote_url"
+
     @Deprecated("Use GIT_REMOTE_URL instead")
     const val GIT_REMOTE_USERNAME = "git_remote_username"
     const val GIT_SERVER_INFO = "git_server_info"
@@ -55,7 +60,13 @@ object PreferenceKeys {
     const val REPO_CHANGED = "repo_changed"
     const val SEARCH_ON_START = "search_on_start"
     const val SHOW_EXTRA_CONTENT = "show_extra_content"
+
+    @Deprecated(
+        message = "Use SHOW_HIDDEN_CONTENTS instead",
+        replaceWith = ReplaceWith("PreferenceKeys.SHOW_HIDDEN_CONTENTS")
+    )
     const val SHOW_HIDDEN_FOLDERS = "show_hidden_folders"
+    const val SHOW_HIDDEN_CONTENTS = "show_hidden_contents"
     const val SORT_ORDER = "sort_order"
     const val SHOW_PASSWORD = "show_password"
     const val SSH_KEY = "ssh_key"

app/src/main/res/values-pt-rBR/strings.xml
@@ -269,8 +269,6 @@
     <string name="access_sdcard_text">O local do armazenamento está em seu cartão SD ou armazenamento interno, mas o aplicativo não tem permissão para acessá-lo.</string>
     <string name="your_public_key">Sua chave pública</string>
     <string name="error_generate_ssh_key">Erro ao tentar gerar a chave SSH</string>
-    <string name="pref_show_hidden_title">Mostrar pastas ocultas</string>
-    <string name="pref_show_hidden_summary">Incluir diretórios ocultos na lista de senhas</string>
     <string name="title_create_folder">Criar pasta</string>
     <string name="title_rename_folder">Renomear pasta</string>
     <string name="message_category_error_empty_field">O nome da categoria não pode ser vazio</string>

commit 12350bfd270c2ec73826e4430d2da165c9b8cdbe

PasswordItemRecyclerView: correctly count children
Revert changes to this file from 1ce3ef4ea3c5 since they were never required.

Fixes: 1ce3ef4ea3c5 ("Expand show hidden folders to also cover files (#1059)")

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/zeapo/pwdstore/ui/adapters/PasswordItemRecyclerAdapter.kt
@@ -17,9 +17,6 @@ import com.zeapo.pwdstore.R
 import com.zeapo.pwdstore.SearchableRepositoryAdapter
 import com.zeapo.pwdstore.stableId
 import com.zeapo.pwdstore.utils.PasswordItem
-import com.zeapo.pwdstore.utils.PreferenceKeys
-import com.zeapo.pwdstore.utils.sharedPrefs
-import java.io.File
 
 open class PasswordItemRecyclerAdapter :
     SearchableRepositoryAdapter<PasswordItemRecyclerAdapter.PasswordItemViewHolder>(
@@ -49,8 +46,6 @@ open class PasswordItemRecyclerAdapter :
         lateinit var itemDetails: ItemDetailsLookup.ItemDetails<String>
 
         fun bind(item: PasswordItem) {
-            val settings = itemView.context.sharedPrefs
-            val showHidden = settings.getBoolean(PreferenceKeys.SHOW_HIDDEN_CONTENTS, false)
             val parentPath = item.fullPathToParent.replace("(^/)|(/$)".toRegex(), "")
             val source = if (parentPath.isNotEmpty()) {
                 "$parentPath\n$item"
@@ -62,10 +57,9 @@ open class PasswordItemRecyclerAdapter :
             name.text = spannable
             if (item.type == PasswordItem.TYPE_CATEGORY) {
                 folderIndicator.visibility = View.VISIBLE
-                val children = with(item.file) {
-                    if (showHidden) listFiles() else listFiles { pathname -> pathname.isDirectory && !pathname.isHidden }
-                } ?: emptyArray<File>()
-                val count = children.size
+                val count = with(item.file) {
+                    listFiles { path -> path.isDirectory || path.extension == "gpg" }
+                }?.size ?: 0
                 childCount.visibility = if (count > 0) View.VISIBLE else View.GONE
                 childCount.text = "$count"
             } else {