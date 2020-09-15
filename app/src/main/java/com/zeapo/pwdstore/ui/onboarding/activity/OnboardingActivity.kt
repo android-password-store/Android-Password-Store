60ca6e990c98a004b42501495ea0d8cb4904fac7
Harsh Shandilya <me@msfjarvis.dev>
2020-09-15 17:54:21 +0530

    onboarding: cleanup OnboardingActivity init
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/ui/onboarding/activity/OnboardingActivity.kt
+++ app/src/main/java/com/zeapo/pwdstore/ui/onboarding/activity/OnboardingActivity.kt
@@ -7,15 +7,13 @@ package com.zeapo.pwdstore.ui.onboarding.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.zeapo.pwdstore.databinding.ActivityOnboardingBinding
+import com.zeapo.pwdstore.R
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity(R.layout.activity_onboarding) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityOnboardingBinding.inflate(layoutInflater)
         supportActionBar?.hide()
-        setContentView(binding.root)
     }
 
     override fun onBackPressed() {
