f64589356c4e44669f4488607a8b1dbe1b52354e
Harsh Shandilya <me@msfjarvis.dev>
2020-09-18 16:48:21 +0530

    FragmentExtensions: remove unused API
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/utils/FragmentExtensions.kt
+++ app/src/main/java/com/zeapo/pwdstore/utils/FragmentExtensions.kt
@@ -1,7 +1,6 @@
 package com.zeapo.pwdstore.utils
 
 import android.content.pm.PackageManager
-import android.view.View
 import androidx.annotation.IdRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -15,20 +14,8 @@ fun Fragment.isPermissionGranted(permission: String): Boolean {
 
 fun Fragment.finish() = requireActivity().finish()
 
-fun FragmentManager.performTransaction(destinationFragment: Fragment, @IdRes containerViewId: Int = android.R.id.content) {
-    this.commit {
-        beginTransaction()
-        setCustomAnimations(
-            R.animator.slide_in_left,
-            R.animator.slide_out_left,
-            R.animator.slide_in_right,
-            R.animator.slide_out_right)
-        replace(containerViewId, destinationFragment)
-    }
-}
-
 fun FragmentManager.performTransactionWithBackStack(destinationFragment: Fragment, @IdRes containerViewId: Int = android.R.id.content) {
-    this.commit {
+    commit {
         beginTransaction()
         addToBackStack(destinationFragment.tag)
         setCustomAnimations(
@@ -36,17 +23,6 @@ fun FragmentManager.performTransactionWithBackStack(destinationFragment: Fragmen
             R.animator.slide_out_left,
             R.animator.slide_in_right,
             R.animator.slide_out_right)
-        replace(containerViewId, destinationFragment)
-    }
-}
-
-fun FragmentManager.performSharedElementTransaction(destinationFragment: Fragment, views: List<View>, @IdRes containerViewId: Int = android.R.id.content) {
-    this.commit {
-        beginTransaction()
-        for (view in views) {
-            addSharedElement(view, view.transitionName)
-        }
-        addToBackStack(destinationFragment.tag)
         replace(containerViewId, destinationFragment)
     }
 }
