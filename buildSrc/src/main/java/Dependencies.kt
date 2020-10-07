30c48e8e2faa4fa395ccef740136887e491abe47
Harsh Shandilya <me@msfjarvis.dev>
2020-10-07 08:32:54 +0530

    build: update runtime dependencies and Gradle plugins
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ private const val KOTLIN_VERSION = "1.4.10"
 
 object Plugins {
 
-    const val agp = "com.android.tools.build:gradle:4.0.1"
+    const val agp = "com.android.tools.build:gradle:4.0.2"
     const val binaryCompatibilityValidator = "org.jetbrains.kotlinx:binary-compatibility-validator:0.2.3"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
 }
@@ -24,15 +24,15 @@ object Dependencies {
 
     object AndroidX {
 
-        private const val lifecycleVersion = "2.3.0-alpha07"
+        private const val lifecycleVersion = "2.3.0-beta01"
 
         const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-beta01"
         const val annotation = "androidx.annotation:annotation:1.1.0"
         const val autofill = "androidx.autofill:autofill:1.1.0-alpha02"
         const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha02"
         const val biometric = "androidx.biometric:biometric:1.1.0-beta01"
-        const val constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.1"
-        const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha03"
+        const val constraint_layout = "androidx.constraintlayout:constraintlayout:2.0.2"
+        const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha04"
         const val documentfile = "androidx.documentfile:documentfile:1.0.1"
         const val fragment_ktx = "androidx.fragment:fragment-ktx:1.3.0-beta01"
         const val lifecycle_common = "androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion"
@@ -40,8 +40,8 @@ object Dependencies {
         const val lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
         const val material = "com.google.android.material:material:1.3.0-alpha02"
         const val preference = "androidx.preference:preference:1.1.1"
-        const val recycler_view = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
-        const val recycler_view_selection = "androidx.recyclerview:recyclerview-selection:1.1.0-rc02"
+        const val recycler_view = "androidx.recyclerview:recyclerview:1.2.0-alpha06"
+        const val recycler_view_selection = "androidx.recyclerview:recyclerview-selection:1.1.0-rc03"
         const val security = "androidx.security:security-crypto:1.1.0-alpha02"
         const val swiperefreshlayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01"
     }
@@ -61,8 +61,8 @@ object Dependencies {
         const val fastscroll = "me.zhanghai.android.fastscroll:library:1.1.4"
         const val jgit = "org.eclipse.jgit:org.eclipse.jgit:3.7.1.201504261725-r"
         const val kotlin_result = "com.michael-bull.kotlin-result:kotlin-result:1.1.9"
-        const val leakcanary = "com.squareup.leakcanary:leakcanary-android:2.4"
-        const val plumber = "com.squareup.leakcanary:plumber-android:2.4"
+        const val leakcanary = "com.squareup.leakcanary:leakcanary-android:2.5"
+        const val plumber = "com.squareup.leakcanary:plumber-android:2.5"
         const val sshj = "com.hierynomus:sshj:0.30.0"
         const val ssh_auth = "org.sufficientlysecure:sshauthentication-api:1.0"
         const val timber = "com.jakewharton.timber:timber:4.7.1"
