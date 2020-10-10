9ba83cbc17c98b08b94d765e35d128e1d96701c4
Harsh Shandilya <me@msfjarvis.dev>
2020-10-10 16:33:39 +0530

    Address review comments
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/LaunchActivity.kt
+++ app/src/main/java/com/zeapo/pwdstore/LaunchActivity.kt
@@ -7,26 +7,29 @@ package com.zeapo.pwdstore
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ProgressBar
 import androidx.core.content.edit
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.github.michaelbull.result.fold
 import com.zeapo.pwdstore.crypto.DecryptActivity
+import com.zeapo.pwdstore.databinding.ActivityLaunchBinding
 import com.zeapo.pwdstore.git.BaseGitActivity
 import com.zeapo.pwdstore.git.config.AuthMode
 import com.zeapo.pwdstore.git.config.GitSettings
 import com.zeapo.pwdstore.utils.BiometricAuthenticator
-import com.zeapo.pwdstore.utils.PasswordRepository.Companion.isInitialized
+import com.zeapo.pwdstore.utils.PasswordRepository
 import com.zeapo.pwdstore.utils.PreferenceKeys
 import com.zeapo.pwdstore.utils.sharedPrefs
+import com.zeapo.pwdstore.utils.viewBinding
 import kotlinx.coroutines.launch
 
 class LaunchActivity : BaseGitActivity() {
 
+    private val binding by viewBinding(ActivityLaunchBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launch)
+        setContentView(binding.root)
         supportActionBar?.hide()
         val prefs = sharedPrefs
         if (prefs.getBoolean(PreferenceKeys.BIOMETRIC_AUTH, false)) {
@@ -56,14 +59,16 @@ class LaunchActivity : BaseGitActivity() {
                 putExtra("FILE_PATH", intent.getStringExtra("FILE_PATH"))
                 putExtra("REPO_PATH", intent.getStringExtra("REPO_PATH"))
                 putExtra("LAST_CHANGED_TIMESTAMP", intent.getLongExtra("LAST_CHANGED_TIMESTAMP", 0L))
-                startPasswordStoreActivity(intent, noAuth)
             }
+            startPasswordStoreActivity(intent, noAuth)
         } else {
             val intent = Intent(this, PasswordStore::class.java)
-            if (isInitialized && !GitSettings.url.isNullOrEmpty() && sharedPrefs.getBoolean(PreferenceKeys.SYNC_ON_LAUNCH, false)) {
-                findViewById<ProgressBar>(R.id.progress_bar).isVisible = true
-                findViewById<ProgressBar>(R.id.sync).isVisible = true
-                runGitOperation(intent)
+            if (PasswordRepository.getRepository(null) != null &&
+                !GitSettings.url.isNullOrEmpty() &&
+                sharedPrefs.getBoolean(PreferenceKeys.SYNC_ON_LAUNCH, false)) {
+                binding.progressBar.isVisible = true
+                binding.sync.isVisible = true
+                runGitOperation { startPasswordStoreActivity(intent, noAuth) }
             } else {
                 startPasswordStoreActivity(intent, noAuth)
             }
@@ -74,14 +79,14 @@ class LaunchActivity : BaseGitActivity() {
         Handler().postDelayed({
             startActivity(intent)
             finish()
-        }, 1000L)
+        }, if (noAuth) 500L else 0L)
     }
 
-    private fun runGitOperation(intent: Intent) = lifecycleScope.launch {
+    private fun runGitOperation(onCompletion: () -> Unit) = lifecycleScope.launch {
         val gitOp = if (GitSettings.authMode == AuthMode.None) GitOp.PULL else GitOp.SYNC
         launchGitOperation(gitOp).fold(
-            success = { startPasswordStoreActivity(intent, false) },
-            failure = { promptOnErrorHandler(it) { startPasswordStoreActivity(intent, false) } },
+            success = { onCompletion.invoke() },
+            failure = { promptOnErrorHandler(it) { onCompletion.invoke() } },
         )
     }
 
