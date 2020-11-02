b14ab9001b883bed713da7252dba088c71caee4d
Harsh Shandilya <me@msfjarvis.dev>
2020-11-03 01:07:17 +0530

    autofill-parser: bump version for breaking API change
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- autofill-parser/build.gradle.kts
+++ autofill-parser/build.gradle.kts
@@ -21,8 +21,8 @@ fun getCredential(type: String): String {
 
 android {
     defaultConfig {
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
         consumerProguardFiles("consumer-rules.pro")
     }
 
