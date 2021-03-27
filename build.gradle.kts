d9e5409a11d3cda745e85c253e115e7c3f03ea64
Harsh Shandilya <me@msfjarvis.dev>
2021-03-27 12:43:01 +0530

    build: force the right annotation version manually
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,21 @@ buildscript { dependencies { classpath(Plugins.ktfmtGradlePlugin) } }
 allprojects { apply(plugin = "com.ncorti.ktfmt.gradle") }
 
 subprojects {
+  // Gradle's automatic resolution fails to handle AndroidX annotation for
+  // some reason so here we simply hack it up to use the correct version manually.
+  val annotationParts = Dependencies.AndroidX.annotation.split(":")
+  val annotationGroup = annotationParts[0]
+  val annotationModule = annotationParts[1]
+  val annotationVersion = annotationParts[2]
   configurations.all {
     resolutionStrategy.dependencySubstitution {
       substitute(module("org.jetbrains.trove4j:trove4j:20160824"))
         .using(module("org.jetbrains.intellij.deps:trove4j:1.0.20200330"))
+    }
+    resolutionStrategy.eachDependency {
+      if (requested.group == annotationGroup && requested.name == annotationModule) {
+        useVersion(annotationVersion)
+      }
     }
   }
 }
