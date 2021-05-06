0f465d4b95c7e05dbd62311a3ba034c6dbcd9b50
Harsh Shandilya <me@msfjarvis.dev>
2021-05-06 12:23:53 +0530

    Upgrade Kotlin and AndroidX dependencies (#1402)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@
 plugins {
   `binary-compatibility-validator`
   `aps-plugin`
+  // Fix for leak in Kotlin Gradle Plugin: https://youtrack.jetbrains.com/issue/KT-46368
+  id("dev.zacsweers.kgp-150-leak-patcher") version "1.0.1"
 }
 
 allprojects { apply(plugin = "com.ncorti.ktfmt.gradle") }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,18 @@ repositories {
   google()
   gradlePluginPortal()
   mavenCentral()
+  // Dokka's transitive kotlinx-html dependency is only published to their Space repo
+  // https://github.com/Kotlin/dokka/releases/tag/v1.4.32
+  maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") {
+    name = "KotlinX HTML Maven"
+    content {
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html")
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html-assembly")
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html-common")
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html-js")
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
+    }
+  }
 }
 
 ktfmt {
@@ -37,9 +49,9 @@ gradlePlugin {
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:4.1.3")
+  implementation("com.android.tools.build:gradle:4.2.0")
   implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.5.0")
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30")
+  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
   implementation("de.undercouch:gradle-download-task:4.1.1")
   implementation("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
