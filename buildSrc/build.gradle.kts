8f4cab491752eeb50f6a4e88d5e5507386ecac61
Harsh Shandilya <me@msfjarvis.dev>
2021-05-22 22:13:29 +0530

    build: upgrade AGP
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -41,7 +41,7 @@ gradlePlugin {
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:4.2.0")
+  implementation("com.android.tools.build:gradle:4.2.1")
   implementation("com.diffplug.spotless:spotless-plugin-gradle:5.12.4")
   implementation("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
   implementation("com.squareup.okhttp3:okhttp:4.9.0")
