4f8896487aedf65f9dc9a3a8186f503fca3f725e
Harsh Shandilya <me@msfjarvis.dev>
2021-07-02 05:11:58 +0530

    crypto-pgp: prepare for instrumentation tests

--- crypto-pgp/build.gradle.kts
+++ crypto-pgp/build.gradle.kts
@@ -5,7 +5,12 @@
 plugins {
   id("com.android.library")
   kotlin("android")
+  kotlin("kapt")
   `aps-plugin`
+}
+
+android {
+  defaultConfig { testInstrumentationRunner = "dev.msfjarvis.aps.test.util.HiltTestRunner" }
 }
 
 dependencies {
@@ -16,4 +21,9 @@ dependencies {
   implementation(libs.dagger.hilt.core)
   implementation(libs.thirdparty.kotlinResult)
   implementation(libs.kotlin.coroutines.core)
+  kaptAndroidTest(libs.dagger.hilt.compiler)
+  androidTestImplementation(libs.testing.hilt.android)
+  androidTestImplementation(libs.bundles.androidTestDependencies)
+  androidTestImplementation(libs.bundles.testDependencies)
+  androidTestImplementation(libs.kotlin.coroutines.test)
 }
