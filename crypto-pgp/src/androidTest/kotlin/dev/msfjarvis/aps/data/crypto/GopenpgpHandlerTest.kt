4573baaa94439a2d19004fa68bc28c126eccd2ab
Harsh Shandilya <me@msfjarvis.dev>
2021-07-05 17:05:07 +0530

    crypto-pgp: add test for KeyManager

--- crypto-pgp/src/androidTest/kotlin/dev/msfjarvis/aps/data/crypto/GopenpgpHandlerTest.kt
+++ crypto-pgp/src/androidTest/kotlin/dev/msfjarvis/aps/data/crypto/GopenpgpHandlerTest.kt
@@ -7,6 +7,9 @@ package dev.msfjarvis.aps.data.crypto
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import com.github.michaelbull.result.get
+import com.github.michaelbull.result.getError
+import com.github.michaelbull.result.unwrap
 import com.proton.Gopenpgp.crypto.Crypto
 import com.proton.Gopenpgp.helper.Helper
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -16,9 +19,12 @@ import dev.msfjarvis.aps.test.util.CryptoConstants
 import javax.inject.Inject
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -29,6 +35,7 @@ public class GopenpgpHandlerTest {
   @get:Rule public val hiltRule: HiltAndroidRule = HiltAndroidRule(this)
 
   @Inject public lateinit var keyFactory: GPGKeyPair.Factory
+  @Inject public lateinit var gopenpgpCryptoHandler: GopenpgpCryptoHandler
 
   private fun getKeyManager(): GPGKeyManager {
     return GPGKeyManager(
@@ -64,5 +71,39 @@ public class GopenpgpHandlerTest {
     val encrypted = Helper.encryptBinaryMessage(key.armoredPublicKey, plainData)
     val decrypted = Helper.decryptBinaryMessage(key.armor(), pass, encrypted)
     assertContentEquals(decrypted, plainData)
+  }
+
+  @Test
+  public fun testKeyManager() {
+    val pass = CryptoConstants.KEY_PASSPHRASE.encodeToByteArray()
+    val plainData = CryptoConstants.PLAIN_TEXT.encodeToByteArray()
+    val armored =
+      ApplicationProvider.getApplicationContext<Context>()
+        .resources
+        .openRawResource(R.raw.private_key)
+        .readBytes()
+        .decodeToString()
+    val keyManager = getKeyManager()
+    runBlockingTest {
+      val result = keyManager.addKey(armored)
+      assertNull(result.getError())
+      val keyID = result.get()
+      assertNotNull(keyID)
+      val keypair = keyManager.findKeyById(keyID).unwrap()
+      val privKey = keypair.getPrivateKey().decodeToString()
+      val pubKey = keypair.getPublicKey().decodeToString()
+      val encrypted =
+        gopenpgpCryptoHandler.encrypt(
+          pubKey,
+          plainData,
+        )
+      val decrypted =
+        gopenpgpCryptoHandler.decrypt(
+          privKey,
+          pass,
+          encrypted,
+        )
+      assertEquals(CryptoConstants.PLAIN_TEXT, decrypted.decodeToString())
+    }
   }
 }
