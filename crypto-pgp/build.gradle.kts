a471e2a5100ed10f74d85d6e1796c2fdd7722495
Aditya Wasan <adityawasan55@gmail.com>
2021-08-14 19:30:38 +0530

    crypto-pgp: prepare module for integration tests
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- crypto-pgp/build.gradle.kts
+++ crypto-pgp/build.gradle.kts
@@ -2,14 +2,24 @@
  * Copyright © 2014-2021 The Android Password Store Authors. All Rights Reserved.
  * SPDX-License-Identifier: GPL-3.0-only
  */
+
 plugins {
   id("com.android.library")
   kotlin("android")
   `aps-plugin`
 }
 
+android {
+  defaultConfig {
+    testApplicationId = "dev.msfjarvis.aps.cryptopgp.test"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+}
+
 dependencies {
   api(projects.cryptoCommon)
   implementation(libs.aps.gopenpgp)
   implementation(libs.dagger.hilt.core)
+  androidTestImplementation(libs.bundles.testDependencies)
+  androidTestImplementation(libs.bundles.androidTestDependencies)
 }
