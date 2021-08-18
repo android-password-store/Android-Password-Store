42220383d3f0c084b39f5c2addb7ebb5ddaea664
Harsh Shandilya <me@msfjarvis.dev>
2021-08-18 10:08:40 +0530

    Bump Gradle and Timber (#1488)
    * build: bump to Gradle 7.2
    
    * gradle: bump to Timber 5.0.1

--- buildSrc/src/main/java/BaseProjectConfig.kt
+++ buildSrc/src/main/java/BaseProjectConfig.kt
@@ -22,9 +22,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  */
 internal fun Project.configureForRootProject() {
   tasks.withType<Wrapper> {
-    gradleVersion = "7.1.1"
-    distributionType = Wrapper.DistributionType.ALL
-    distributionSha256Sum = "9bb8bc05f562f2d42bdf1ba8db62f6b6fa1c3bf6c392228802cc7cb0578fe7e0"
+    gradleVersion = "7.2"
+    distributionSha256Sum = "f581709a9c35e9cb92e16f585d2c4bc99b2b1a5f85d2badbd3dc6bff59e1e6dd"
   }
   configureBinaryCompatibilityValidator()
 }
