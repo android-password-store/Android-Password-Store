4a58856d1e9e8f994e288e590710b5105fb2e299
Aditya Wasan <adityawasan55@gmail.com>
2021-10-06 16:37:18 +0530

    Bump kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -53,6 +53,6 @@ dependencies {
   implementation("com.vdurmont:semver4j:3.1.0")
   implementation("de.undercouch:gradle-download-task:4.1.2")
   implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
   implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
 }
