494cb5fc4fbe90d7a4b27e1d1114cb20840883d3
Harsh Shandilya <me@msfjarvis.dev>
2021-11-01 11:11:53 +0530

    scripts/pre-push-hook: skip checks when deleting a branch

--- scripts/pre-push-hook.sh
+++ scripts/pre-push-hook.sh
@@ -4,6 +4,11 @@ set -e
 set -u
 set -o pipefail
 
+ZERO=$(git hash-object --stdin </dev/null | tr '[0-9a-f]' '0')
 GRADLE_EXEC="${GRADLE_EXEC:-./gradlew}"
 
-"${GRADLE_EXEC}" spotlessCheck apiCheck
+while read local_ref local_oid remote_ref remote_oid; do
+  if [ "${local_oid}" != "${ZERO}" ]; then
+    "${GRADLE_EXEC}" spotlessCheck apiCheck
+  fi
+done
