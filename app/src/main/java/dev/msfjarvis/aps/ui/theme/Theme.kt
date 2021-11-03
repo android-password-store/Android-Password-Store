2cf3ddf2aadad011f62ecafbbbf939fd0104cb43
Aditya Wasan <adityawasan55@gmail.com>
2021-11-03 18:09:38 +0530

    Revert "app: add dynamic colors support to Compose"
    This reverts commit 2a9d2177a5985a76020a5dc6b9248343d18e26bc.

--- app/src/main/java/dev/msfjarvis/aps/ui/theme/Theme.kt
+++ app/src/main/java/dev/msfjarvis/aps/ui/theme/Theme.kt
@@ -1,11 +1,8 @@
 package dev.msfjarvis.aps.ui.theme
 
-import android.content.Context
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
@@ -67,16 +64,12 @@ private val DarkThemeColors =
   )
 
 @Composable
-fun APSTheme(
-  useDynamicColors: Boolean = true,
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
-  context: Context,
-  content: @Composable () -> Unit,
-) {
+fun APSTheme(useDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
   val colors =
-    when (useDarkTheme) {
-      true -> if (useDynamicColors) dynamicDarkColorScheme(context) else DarkThemeColors
-      false -> if (useDynamicColors) dynamicLightColorScheme(context) else LightThemeColors
+    if (!useDarkTheme) {
+      LightThemeColors
+    } else {
+      DarkThemeColors
     }
 
   MaterialTheme(colorScheme = colors, typography = AppTypography, content = content)
