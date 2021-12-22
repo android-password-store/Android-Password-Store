126bed1a8b04a788fabc76dfa9037e10f6f71140
Harsh Shandilya <me@msfjarvis.dev>
2021-12-22 21:47:19 +0530

    Upgrade to Gradle 7.3.3 (#1600)
    * gradle: upgrade to 7.3.3
    
    * build-logic: make Kotlin and Java targets match

--- build-logic/automation-plugins/build.gradle.kts
+++ build-logic/automation-plugins/build.gradle.kts
@@ -3,9 +3,23 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   `kotlin-dsl`
   `kotlin-dsl-precompiled-script-plugins`
+}
+
+tasks.withType<JavaCompile>().configureEach {
+  sourceCompatibility = JavaVersion.VERSION_11.toString()
+  targetCompatibility = JavaVersion.VERSION_11.toString()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+  kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 }
 
 gradlePlugin {
