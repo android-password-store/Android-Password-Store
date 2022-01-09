6c6ade85a8696142b5fc13df9034f73b4089c912
Harsh Shandilya <me@msfjarvis.dev>
2022-01-09 15:16:51 +0530

    pre-push-hook: run tests on push (#1653)

--- scripts/pre-push-hook.sh
+++ scripts/pre-push-hook.sh
@@ -9,6 +9,6 @@ GRADLE_EXEC="${GRADLE_EXEC:-./gradlew}"
 
 while read local_ref local_oid remote_ref remote_oid; do
   if [ "${local_oid}" != "${ZERO}" ]; then
-    "${GRADLE_EXEC}" spotlessCheck apiCheck
+    "${GRADLE_EXEC}" spotlessCheck apiCheck test -PslimTests
   fi
 done
