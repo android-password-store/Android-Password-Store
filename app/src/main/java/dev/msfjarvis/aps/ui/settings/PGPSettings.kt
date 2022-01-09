771bce0124db6ab1f6f7f8e06f4a1d7a5d16829e
Harsh Shandilya <me@msfjarvis.dev>
2022-01-09 17:00:54 +0530

    Add a settings entry for PGPainless feature flag

--- app/src/main/java/dev/msfjarvis/aps/ui/settings/PGPSettings.kt
+++ app/src/main/java/dev/msfjarvis/aps/ui/settings/PGPSettings.kt
@@ -7,15 +7,21 @@ package dev.msfjarvis.aps.ui.settings
 
 import androidx.fragment.app.FragmentActivity
 import de.Maxr1998.modernpreferences.PreferenceScreen
+import de.Maxr1998.modernpreferences.helpers.checkBox
 import de.Maxr1998.modernpreferences.helpers.onClick
 import de.Maxr1998.modernpreferences.helpers.pref
 import dev.msfjarvis.aps.ui.pgp.PGPKeyImportActivity
 import dev.msfjarvis.aps.util.extensions.launchActivity
+import dev.msfjarvis.aps.util.features.Feature
 
 class PGPSettings(private val activity: FragmentActivity) : SettingsProvider {
 
   override fun provideSettings(builder: PreferenceScreen.Builder) {
     builder.apply {
+      checkBox(Feature.EnablePGPainlessBackend.configKey) {
+        title = "Enable new PGP backend"
+        persistent = true
+      }
       pref("_") {
         title = "Import PGP key"
         persistent = false
