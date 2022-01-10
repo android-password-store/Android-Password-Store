a9c7ffb2640850c9d52f56652db880efda6f7d11
Harsh Shandilya <me@msfjarvis.dev>
2022-01-10 17:13:08 +0530

    Remove unused class

--- app/src/main/java/dev/msfjarvis/aps/util/FeatureFlags.kt
+++
@@ -1,6 +0,0 @@
-package dev.msfjarvis.aps.util
-
-/** Naive feature flagging functionality to allow merging incomplete features */
-object FeatureFlags {
-  const val ENABLE_PGP_V2_BACKEND = false
-}
