e6c35495381d23ca1b878c3b19435b19ef9174fd
Aditya Wasan <adityawasan55@gmail.com>
2022-01-13 20:53:09 +0530

    feat(random): add exceptions for PasswordGenerator
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ passgen/random/src/main/kotlin/dev/msfjarvis/aps/passgen/random/PasswordGeneratorException.kt
@@ -0,0 +1,9 @@
+package dev.msfjarvis.aps.passgen.random
+
+public sealed class PasswordGeneratorException(message: String? = null, cause: Throwable? = null) : Throwable(message, cause)
+
+public class MaxIterationsExceededException(): PasswordGeneratorException()
+
+public class NoCharactersIncludedException(): PasswordGeneratorException()
+
+public class PasswordLengthTooShortException(): PasswordGeneratorException()
