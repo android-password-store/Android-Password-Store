android-password-store/Android-Password-Store

commit b8ce4f314538749384b65201bd6ed0bd301a81dc

app: add `PendingIntent.FLAG_IMMUTABLE` flag where required

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillDecryptActivity.kt
@@ -79,7 +79,7 @@ class AutofillDecryptActivity : AppCompatActivity() {
           context,
           decryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillDecryptActivityV2.kt
@@ -67,7 +67,7 @@ class AutofillDecryptActivityV2 : AppCompatActivity() {
           context,
           decryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillFilterView.kt
@@ -74,7 +74,7 @@ class AutofillFilterView : AppCompatActivity() {
           context,
           matchAndDecryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillPublisherChangedActivity.kt
@@ -54,7 +54,7 @@ class AutofillPublisherChangedActivity : AppCompatActivity() {
           context,
           publisherChangedRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/util/autofill/AutofillViewUtils.kt
@@ -52,7 +52,12 @@ fun makeInlinePresentation(
   if (UiVersions.INLINE_UI_VERSION_1 !in UiVersions.getVersions(imeSpec.style)) return null
 
   val launchIntent =
-    PendingIntent.getActivity(context, 0, Intent(context, PasswordStore::class.java), 0)
+    PendingIntent.getActivity(
+      context,
+      0,
+      Intent(context, PasswordStore::class.java),
+      PendingIntent.FLAG_IMMUTABLE
+    )
   val slice =
     InlineSuggestionUi.newContentBuilder(launchIntent).run {
       setTitle(metadata.title)

app/src/main/java/dev/msfjarvis/aps/util/services/ClipboardService.kt
@@ -115,7 +115,12 @@ class ClipboardService : Service() {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         PendingIntent.getForegroundService(this, 0, clearIntent, PendingIntent.FLAG_UPDATE_CURRENT)
       } else {
-        PendingIntent.getService(this, 0, clearIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        PendingIntent.getService(
+          this,
+          0,
+          clearIntent,
+          PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
       }
     val notification =
       if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {

commit e35508937a075b948b3beca886daff93a7ae95df

app: make Autofill intents mutable
Fixes: b8ce4f314538 ("app: add `PendingIntent.FLAG_IMMUTABLE` flag where required")

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillDecryptActivity.kt
@@ -79,7 +79,7 @@ class AutofillDecryptActivity : AppCompatActivity() {
           context,
           decryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillDecryptActivityV2.kt
@@ -67,7 +67,7 @@ class AutofillDecryptActivityV2 : AppCompatActivity() {
           context,
           decryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillFilterView.kt
@@ -74,7 +74,7 @@ class AutofillFilterView : AppCompatActivity() {
           context,
           matchAndDecryptFileRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/ui/autofill/AutofillPublisherChangedActivity.kt
@@ -54,7 +54,7 @@ class AutofillPublisherChangedActivity : AppCompatActivity() {
           context,
           publisherChangedRequestCode++,
           intent,
-          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+          PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE,
         )
         .intentSender
     }

app/src/main/java/dev/msfjarvis/aps/util/autofill/AutofillViewUtils.kt
@@ -56,7 +56,7 @@ fun makeInlinePresentation(
       context,
       0,
       Intent(context, PasswordStore::class.java),
-      PendingIntent.FLAG_IMMUTABLE
+      PendingIntent.FLAG_MUTABLE
     )
   val slice =
     InlineSuggestionUi.newContentBuilder(launchIntent).run {

app/src/main/java/dev/msfjarvis/aps/util/services/ClipboardService.kt
@@ -119,7 +119,7 @@ class ClipboardService : Service() {
           this,
           0,
           clearIntent,
-          PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+          PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
         )
       }
     val notification =