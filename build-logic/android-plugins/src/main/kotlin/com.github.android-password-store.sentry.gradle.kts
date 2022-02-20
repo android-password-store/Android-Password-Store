bb7d25dd5c4cc27f6a3bba2807465036c1b87c17
Harsh Shandilya <me@msfjarvis.dev>
2022-02-20 13:09:41 +0530

    app: add Sentry and configure a manifest placeholder for it

---
+++ build-logic/android-plugins/src/main/kotlin/com.github.android-password-store.sentry.gradle.kts
@@ -0,0 +1,29 @@
+@file:Suppress("PropertyName")
+
+import flavors.FlavorDimensions
+import flavors.ProductFlavors
+
+plugins { id("com.android.application") }
+
+val SENTRY_DSN_PROPERTY = "SENTRY_DSN"
+val INVOKED_FROM_IDE_PROPERTY = "android.injected.invoked.from.ide"
+
+android {
+  androidComponents {
+    onVariants(selector().withFlavor(FlavorDimensions.FREE to ProductFlavors.NON_FREE)) { variant ->
+      val sentryDsn =
+        project
+          .providers
+          .environmentVariable(SENTRY_DSN_PROPERTY)
+          .orElse("https://public_key@example.com/project_id")
+      if (sentryDsn.isPresent) {
+        variant.manifestPlaceholders.put("sentryDsn", sentryDsn.get())
+      } else if (project.providers.gradleProperty(INVOKED_FROM_IDE_PROPERTY).orNull != "true") {
+        // Checking for 'INVOKED_FROM_IDE_PROPERTY' prevents failures during Gradle sync by the IDE
+        throw GradleException(
+          "The '${SENTRY_DSN_PROPERTY}' environment variable must be set when building the ${ProductFlavors.NON_FREE} flavor"
+        )
+      }
+    }
+  }
+}
