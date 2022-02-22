7aaf4cba1985986d532653d5e1439132f5e96e25
Harsh Shandilya <me@msfjarvis.dev>
2022-02-22 13:59:00 +0530

    crypto-pgpainless: assert key ID for multiple identities

--- crypto-pgpainless/src/test/kotlin/dev/msfjarvis/aps/crypto/KeyUtilsTest.kt
+++ crypto-pgpainless/src/test/kotlin/dev/msfjarvis/aps/crypto/KeyUtilsTest.kt
@@ -4,6 +4,7 @@ import dev.msfjarvis.aps.crypto.KeyUtils.tryGetId
 import dev.msfjarvis.aps.crypto.KeyUtils.tryParseKeyring
 import dev.msfjarvis.aps.crypto.TestUtils.getArmoredPrivateKeyWithMultipleIdentities
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 import org.bouncycastle.openpgp.PGPSecretKeyRing
@@ -18,5 +19,6 @@ class KeyUtilsTest {
     val keyId = tryGetId(key)
     assertNotNull(keyId)
     assertIs<GpgIdentifier.KeyId>(keyId)
+    assertEquals("b950ae2813841585", keyId.toString())
   }
 }
