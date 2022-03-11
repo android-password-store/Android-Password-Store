dc1c139d031331f1bd5f3c4af4924c939b246cc3
Harsh Shandilya <me@msfjarvis.dev>
2022-03-11 16:21:37 +0530

    build-logic: remove error path in Sentry plugin

--- build-logic/android-plugins/src/main/kotlin/com.github.android-password-store.sentry.gradle.kts
+++ build-logic/android-plugins/src/main/kotlin/com.github.android-password-store.sentry.gradle.kts
@@ -6,7 +6,6 @@ import flavors.ProductFlavors
 plugins { id("com.android.application") }
 
 val SENTRY_DSN_PROPERTY = "SENTRY_DSN"
-val INVOKED_FROM_IDE_PROPERTY = "android.injected.invoked.from.ide"
 
 android {
   androidComponents {
@@ -14,11 +13,6 @@ android {
       val sentryDsn = project.providers.environmentVariable(SENTRY_DSN_PROPERTY)
       if (sentryDsn.isPresent) {
         variant.manifestPlaceholders.put("sentryDsn", sentryDsn.get())
-      } else if (project.providers.gradleProperty(INVOKED_FROM_IDE_PROPERTY).orNull != "true") {
-        // Checking for 'INVOKED_FROM_IDE_PROPERTY' prevents failures during Gradle sync by the IDE
-        throw GradleException(
-          "The '${SENTRY_DSN_PROPERTY}' environment variable must be set when building the ${ProductFlavors.NON_FREE} flavor"
-        )
       }
     }
   }
