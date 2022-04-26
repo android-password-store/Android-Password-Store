84755a372160b1aece46f4d6357a2caddc727224
Harsh Shandilya <me@msfjarvis.dev>
2022-04-26 13:17:30 +0530

    build-logic: remove explicit Dokka dependency (#1873)

--- build-logic/android-plugins/build.gradle.kts
+++ build-logic/android-plugins/build.gradle.kts
@@ -32,7 +32,6 @@ gradlePlugin {
 
 dependencies {
   implementation(libs.build.agp)
-  implementation(libs.build.dokka)
   implementation(libs.build.mavenpublish)
   implementation(libs.build.semver)
   implementation(libs.build.sentry)
