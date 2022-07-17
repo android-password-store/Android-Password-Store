831304a7bc5dd803ff0fce84663d04a40133484f
Harsh Shandilya <me@msfjarvis.dev>
2022-07-17 13:54:51 +0530

    scripts/pre-push: add detekt

--- scripts/pre-push-hook.sh
+++ scripts/pre-push-hook.sh
@@ -9,6 +9,6 @@ GRADLE_EXEC="${GRADLE_EXEC:-./gradlew}"
 
 while read local_ref local_oid remote_ref remote_oid; do
   if [ "${local_oid}" != "${ZERO}" ]; then
-    "${GRADLE_EXEC}" spotlessCheck apiCheck test -PslimTests
+    "${GRADLE_EXEC}" apiCheck detekt spotlessCheck test -PslimTests
   fi
 done
