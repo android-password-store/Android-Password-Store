9f5062986d9240ffb5005e901acc14471e7ada8b
Harsh Shandilya <me@msfjarvis.dev>
2022-10-10 14:45:07 +0530

    Revert "chore: remove unnecessary `CryptoHandlerModule`"
    This is used in #2170 so let's avoid the unnecessary conflict.
    
    This reverts commit f5bf45037a57b431dd693a29ac41217576a7d437.

---
+++ app/src/main/java/app/passwordstore/injection/crypto/CryptoHandlerModule.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2014-2021 The Android Password Store Authors. All Rights Reserved.
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package app.passwordstore.injection.crypto
+
+import app.passwordstore.crypto.PGPainlessCryptoHandler
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CryptoHandlerModule {
+  @Provides fun providePgpCryptoHandler() = PGPainlessCryptoHandler()
+}
