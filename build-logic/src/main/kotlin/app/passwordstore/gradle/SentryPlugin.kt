5cccd535aec307d2b55a51091f0119a5055407db
Harsh Shandilya <me@msfjarvis.dev>
2022-12-29 22:30:01 +0530

    Revert "fix: disable Sentry dependency report"
    This reverts commit 5c35acdfbcffdacb2e35b51dcbd8e279db44c5ae.

--- build-logic/src/main/kotlin/app/passwordstore/gradle/SentryPlugin.kt
+++ build-logic/src/main/kotlin/app/passwordstore/gradle/SentryPlugin.kt
@@ -33,7 +33,6 @@ class SentryPlugin : Plugin<Project> {
         ignoredFlavors.set(setOf(ProductFlavors.FREE))
         tracingInstrumentation { enabled.set(false) }
         autoInstallation.enabled.set(false)
-        includeDependenciesReport.set(false)
       }
     }
   }
