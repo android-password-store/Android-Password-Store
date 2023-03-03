3e56fa2e120db21676074e15da80eea3337df4e0
Harsh Shandilya <me@msfjarvis.dev>
2023-03-04 02:01:15 +0530

    refactor: use worker API for `KtfmtFormatTask`

--- build-logic/src/main/kotlin/app/passwordstore/gradle/KtfmtPlugin.kt
+++ build-logic/src/main/kotlin/app/passwordstore/gradle/KtfmtPlugin.kt
@@ -2,6 +2,7 @@ package app.passwordstore.gradle
 
 import app.passwordstore.gradle.ktfmt.KtfmtCheckTask
 import app.passwordstore.gradle.ktfmt.KtfmtFormatTask
+import java.util.concurrent.Callable
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.register
@@ -9,23 +10,14 @@ import org.gradle.kotlin.dsl.register
 class KtfmtPlugin : Plugin<Project> {
 
   override fun apply(target: Project) {
-    target.tasks.register<KtfmtFormatTask>("ktfmtFormat") {
-      source =
-        project.layout.projectDirectory.asFileTree
-          .filter { file ->
-            file.extension == "kt" ||
-              file.extension == "kts" && !file.canonicalPath.contains("build")
-          }
-          .asFileTree
+    val input = Callable {
+      target.layout.projectDirectory.asFileTree.filter { file ->
+        file.extension == "kt" || file.extension == "kts" && !file.canonicalPath.contains("build")
+      }
     }
+    target.tasks.register<KtfmtFormatTask>("ktfmtFormat") { source(input) }
     target.tasks.register<KtfmtCheckTask>("ktfmtCheck") {
-      source =
-        project.layout.projectDirectory.asFileTree
-          .filter { file ->
-            file.extension == "kt" ||
-              file.extension == "kts" && !file.canonicalPath.contains("build")
-          }
-          .asFileTree
+      source(input)
       projectDirectory.set(target.layout.projectDirectory)
     }
   }
--- build-logic/src/main/kotlin/app/passwordstore/gradle/ktfmt/KtfmtFormatTask.kt
+++ build-logic/src/main/kotlin/app/passwordstore/gradle/ktfmt/KtfmtFormatTask.kt
@@ -1,52 +1,53 @@
 package app.passwordstore.gradle.ktfmt
 
-import com.facebook.ktfmt.format.Formatter
-import com.facebook.ktfmt.format.FormattingOptions
-import java.io.File
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
-import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.IgnoreEmptyDirectories
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
+import javax.inject.Inject
+import org.gradle.api.GradleException
+import org.gradle.api.file.ProjectLayout
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.internal.exceptions.MultiCauseException
+import org.gradle.workers.WorkerExecutor
+import org.jetbrains.kotlin.utils.addToStdlib.ifNotEmpty
 
-@OptIn(ExperimentalCoroutinesApi::class)
-abstract class KtfmtFormatTask : SourceTask() {
+abstract class KtfmtFormatTask
+@Inject
+constructor(
+  private val workerExecutor: WorkerExecutor,
+  private val projectLayout: ProjectLayout,
+) : SourceTask() {
 
-  @get:PathSensitive(PathSensitivity.RELATIVE)
-  @get:InputFiles
-  @get:IgnoreEmptyDirectories
-  protected val inputFiles: FileCollection
-    get() = super.getSource()
+  init {
+    outputs.upToDateWhen { false }
+  }
 
   @TaskAction
   fun execute() {
-    runBlocking(Dispatchers.IO.limitedParallelism(PARALLEL_TASK_LIMIT)) {
-      coroutineScope { inputFiles.map { async { formatFile(it) } }.awaitAll() }
+    val result =
+      with(workerExecutor.noIsolation()) {
+        submit(KtfmtWorkerAction::class.java) {
+          name.set("foofoo")
+          files.from(source)
+          projectDirectory.set(projectLayout.projectDirectory.asFile)
+        }
+        runCatching { await() }
+      }
+
+    result.exceptionOrNull()?.workErrorCauses<Exception>()?.ifNotEmpty {
+      forEach { logger.error(it.message, it.cause) }
+      throw GradleException("error formatting sources for $name")
     }
   }
 
-  private fun formatFile(input: File) {
-    val originCode = input.readText()
-    val formattedCode =
-      Formatter.format(
-        FormattingOptions(
-          style = FormattingOptions.Style.GOOGLE,
-          maxWidth = 100,
-          continuationIndent = 2,
-        ),
-        originCode
-      )
-    if (originCode != formattedCode) {
-      input.writeText(formattedCode)
-    }
+  private inline fun <reified T : Throwable> Throwable.workErrorCauses(): List<Throwable> {
+    return when (this) {
+        is MultiCauseException -> this.causes.map { it.cause }
+        else -> listOf(this.cause)
+      }
+      .filter {
+        // class instance comparison doesn't work due to different classloaders
+        it?.javaClass?.canonicalName == T::class.java.canonicalName
+      }
+      .filterNotNull()
   }
 
   companion object {
