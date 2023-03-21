a6652134a252180c5ab2808b80146393745abe45
Harsh Shandilya <me@msfjarvis.dev>
2023-03-21 11:34:13 +0530

    fix(build): remove unneeded `afterEvaluate`
    https://docs.gradle.org/8.1-rc-1/release-notes.html#:~:text=Easier%20customization%20of%20Kotlin%20options

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -11,20 +11,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins { `kotlin-dsl` }
 
-afterEvaluate {
-  tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = JavaVersion.VERSION_11.toString()
-    targetCompatibility = JavaVersion.VERSION_11.toString()
-  }
+tasks.withType<JavaCompile>().configureEach {
+  sourceCompatibility = JavaVersion.VERSION_11.toString()
+  targetCompatibility = JavaVersion.VERSION_11.toString()
+}
 
-  tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-      jvmTarget = JavaVersion.VERSION_11.toString()
-      freeCompilerArgs = freeCompilerArgs + listOf(
-        "-Xsam-conversions=class",
-        "-opt-in=kotlin.RequiresOptIn",
-      )
-    }
+tasks.withType<KotlinCompile>().configureEach {
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+    freeCompilerArgs = freeCompilerArgs + listOf(
+      "-Xsam-conversions=class",
+      "-opt-in=kotlin.RequiresOptIn",
+    )
   }
 }
 
