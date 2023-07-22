bed8ec76e33c165aa23d48c5f88df1409a9ce2bb
Harsh Shandilya <me@msfjarvis.dev>
2023-07-22 15:47:41 +0530

    fix(ssh): remove usage of `kotlin.runCatching`

--- ssh/src/main/kotlin/app/passwordstore/ssh/provider/KeystoreNativeKeyProvider.kt
+++ ssh/src/main/kotlin/app/passwordstore/ssh/provider/KeystoreNativeKeyProvider.kt
@@ -3,6 +3,8 @@ package app.passwordstore.ssh.provider
 import app.passwordstore.ssh.utils.Constants.KEYSTORE_ALIAS
 import app.passwordstore.ssh.utils.sshPrivateKey
 import app.passwordstore.ssh.utils.sshPublicKey
+import com.github.michaelbull.result.getOrElse
+import com.github.michaelbull.result.runCatching
 import java.io.IOException
 import java.security.KeyStore
 import java.security.PrivateKey
