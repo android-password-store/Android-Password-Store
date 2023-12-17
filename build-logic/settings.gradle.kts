600e20824eee63cbbd57110bdf84737aab40dd3d
Harsh Shandilya <me@msfjarvis.dev>
2023-12-17 14:42:40 +0530

    refactor(build): simplify repository declarations

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -10,30 +10,11 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 
 dependencyResolutionManagement {
   repositories {
-    exclusiveContent {
-      forRepository { google() }
-      filter {
+    google {
+      content {
         includeGroup("androidx.databinding")
-        includeGroup("com.android")
-        includeGroup("com.android.tools.analytics-library")
-        includeGroup("com.android.tools.build")
-        includeGroup("com.android.tools.build.jetifier")
-        includeGroup("com.android.databinding")
-        includeGroup("com.android.tools.ddms")
-        includeGroup("com.android.tools.layoutlib")
-        includeGroup("com.android.tools.lint")
-        includeGroup("com.android.tools.utp")
+        includeGroupByRegex("com.android.*")
         includeGroup("com.google.testing.platform")
-        includeModule("com.android.tools", "annotations")
-        includeModule("com.android.tools", "common")
-        includeModule("com.android.tools", "desugar_jdk_libs")
-        includeModule("com.android.tools", "desugar_jdk_libs_configuration")
-        includeModule("com.android.tools", "dvlib")
-        includeModule("com.android.tools", "play-sdk-proto")
-        includeModule("com.android.tools", "repository")
-        includeModule("com.android.tools", "sdklib")
-        includeModule("com.android.tools", "sdk-common")
-        includeModule("com.android.tools.metalava", "metalava")
       }
     }
     exclusiveContent {
